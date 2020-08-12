c9adce273aa171d2859281d581578c9e80853076
Pavlo Khomiak <pavlo.khomiak93@gmail.com>
2020-08-12 17:26:07 +0300

    Add solution

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,6 +1,52 @@
 package core.basesyntax;
 
-public class Engine {
+import java.util.Objects;
+
+public class Engine implements Cloneable {
     private int horsePower;
     private String maker;
+
+    public Engine(int horsePower, String maker) {
+        this.horsePower = horsePower;
+        this.maker = maker;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
+    public void setMaker(String maker) {
+        this.maker = maker;
+    }
+
+    @Override
+    protected Engine clone() {
+        return new Engine(horsePower, maker);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Engine engine = (Engine) o;
+        return horsePower == engine.horsePower
+                && Objects.equals(maker, engine.maker);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(horsePower, maker);
+    }
 }
