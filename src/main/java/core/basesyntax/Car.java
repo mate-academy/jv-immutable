fde5e8f6d8b12350379d8c66e0e2b9d76caa7a42
Dm_Grt <grytsishin@gmail.com>
2020-08-12 17:54:31 +0300

    Car class is made immutable

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,13 +1,82 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-/**
- * Make this class immutable. See requirements in the README file
- */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
+
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+
+        List<Wheel> newWheels = new ArrayList<>();
+        for (Wheel wheel : wheels) {
+            newWheels.add(new Wheel(wheel.getRadius()));
+        }
+        this.wheels = newWheels(wheels);
+        this.engine = engine == null ? null : new Engine(engine.getHorsePower(), engine.getMaker());
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        List<Wheel> newWheels = new ArrayList<>();
+        for (Wheel wheel : wheels) {
+            newWheels.add(new Wheel(wheel.getRadius()));
+        }
+        return newWheels;
+    }
+
+    public Engine getEngine() {
+        return engine == null ? null : new Engine(engine.getHorsePower(), engine.getMaker());
+    }
+
+    public Car changeColor(String color) {
+        return new Car(getYear(), color, getWheels(), getEngine());
+    }
+
+    public Car addWheel(Wheel wheel) {
+        List<Wheel> newWheels = new ArrayList<>(wheels);
+        newWheels.add(new Wheel(wheel.getRadius()));
+        return new Car(getYear(), getColor(), newWheels, getEngine());
+    }
+
+    public Car changeEngine(Engine engine) {
+        return new Car(getYear(), getColor(), getWheels(), engine);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Car)) return false;
+        Car car = (Car) o;
+        return getYear() == car.getYear() &&
+                Objects.equals(getColor(), car.getColor()) &&
+                Objects.equals(getWheels(), car.getWheels()) &&
+                Objects.equals(getEngine(), car.getEngine());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getYear(), getColor(), getWheels(), getEngine());
+    }
+
+    private List<Wheel> newWheels(List<Wheel> wheels) {
+        List<Wheel> newWheels = new ArrayList<>();
+        for (Wheel wheel : wheels) {
+            newWheels.add(new Wheel(wheel.getRadius()));
+        }
+        return newWheels;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,6 +1,48 @@
 package core.basesyntax;
 
-public class Engine {
+import java.util.Objects;
+
+public class Engine implements Cloneable {
     private int horsePower;
     private String maker;
+
+    public Engine(int horsePower, String maker) {
+        this.horsePower = horsePower;
+        this.maker = maker;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
+    public void setMaker(String maker) {
+        this.maker = maker;
+    }
+
+    @Override
+    public Engine clone() {
+        return new Engine(horsePower, maker);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Engine)) return false;
+        Engine engine = (Engine) o;
+        return getHorsePower() == engine.getHorsePower() &&
+                Objects.equals(getMaker(), engine.getMaker());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getHorsePower(), getMaker());
+    }
 }
