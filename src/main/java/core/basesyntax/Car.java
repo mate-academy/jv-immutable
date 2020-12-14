2889b023f60d995920500846b720875cbf27a510
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-14 19:51:25 +0200

    added equals and hashcode to Car class

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,21 +1,23 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Make this class immutable. See requirements in the README file
  */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
 
     public Car(int year, String color, List<Wheel> wheels, Engine engine) {
         this.year = year;
         this.color = color;
-        this.wheels = wheels;
-        this.engine = engine;
+        this.wheels = new ArrayList<>(wheels);
+        this.engine = engine.clone();
     }
 
     public Car changeEngine(Engine engine) {
@@ -39,10 +41,26 @@ public class Car {
     }
 
     public List<Wheel> getWheels() {
-        return wheels;
+        return new ArrayList<>(wheels);
     }
 
     public Engine getEngine() {
-        return engine;
+        return engine.clone();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o.getClass().equals(this.getClass()))) return false;
+        Car car = (Car) o;
+        return year == car.year &&
+                Objects.equals(color, car.color) &&
+                Objects.equals(wheels, car.wheels) &&
+                Objects.equals(engine, car.engine);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(year, color, wheels, engine);
     }
 }
