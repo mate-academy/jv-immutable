237d9aedd4bb45d2874f3d5c6c41dd8c73c8ed8e
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-14 19:15:31 +0200

    class engine was implemented

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,6 +1,36 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable{
     private int horsePower;
     private String manufacturer;
+
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    protected Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't clone the engine", e);
+        }
+    }
 }
