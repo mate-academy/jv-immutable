2e161355e34d53543b5854a66c73a26650dd2dc7
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-14 19:07:29 +0200

    added parametrized constructor and getters

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -10,4 +10,27 @@ public class Car {
     private String color;
     private List<Wheel> wheels;
     private Engine engine;
+
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = wheels;
+        this.engine = engine;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        return wheels;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
 }
