18af2442fece7c3052a2674629b3f1069825ae42
Rostyslav Mandybur <mandyburostyk@gmail.com>
2020-12-17 14:01:22 +0200

    change some arguments in constructors

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -35,7 +35,7 @@ public final class Car {
 
     public Car changeEngine(Engine engine) {
 
-        return new Car(year, color, getWheels(), engine);
+        return new Car(year, color, wheels, engine);
     }
 
     public Car changeColor(String newColor) {
