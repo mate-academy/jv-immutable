00c7807ba343b1022c10000ec1452add978d069a
Alekearl <alexandrhurko@gmail.com>
2020-12-20 23:20:31 +0200

    created class Car

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,13 +1,94 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Make this class immutable. See requirements in the README file
  */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car implements Cloneable {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
+
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = wheelList(wheels);
+        this.engine = engine != null ? engine.clone() : null;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        return wheelList(wheels);
+    }
+
+    public Engine getEngine() {
+        return engine != null ? engine.clone() : null;
+    }
+
+    public Car changeColor(String newColor) {
+        Car carColor = new Car(year, newColor, wheels, engine);
+        return carColor;
+    }
+
+    public Car addWheel(Wheel newWheel) {
+        Car carNewWheel = new Car(year, color, Collections.singletonList(newWheel), engine);
+        return carNewWheel;
+    }
+
+    public Car changeEngine(Engine engine) {
+        Car carEngine = new Car(year, color, wheels, engine);
+        return carEngine;
+    }
+
+    public List<Wheel> wheelList(List<Wheel> wheels) {
+        List<Wheel> list = new ArrayList<>();
+        for (Wheel oneWheel : wheels) {
+            list.add(oneWheel.clone());
+        }
+        return list;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Car car = (Car) o;
+
+        if (year != car.year) {
+            return false;
+        }
+        if (!Objects.equals(color, car.color)) {
+            return false;
+        }
+        if (!Objects.equals(wheels, car.wheels)) {
+            return false;
+        }
+        return Objects.equals(engine, car.engine);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = year;
+        result = 31 * result + (color != null ? color.hashCode() : 0);
+        result = 31 * result + (wheels != null ? wheels.hashCode() : 0);
+        result = 31 * result + (engine != null ? engine.hashCode() : 0);
+        return result;
+    }
 }
