3dce9dcb46de581d240fc7be5b35260baf27eb52
antonpanchishko <PanchishkoMayor@gmail.com>
2020-12-20 14:26:47 +0200

    1) implemented immutable class

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,13 +1,76 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Make this class immutable. See requirements in the README file
  */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
+
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = createClonedListOfWheels(wheels);
+        this.engine = engine == null ? null : engine.clone();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return year == car.year && Objects.equals(color, car.color)
+                && Objects.equals(wheels, car.wheels)
+                && Objects.equals(engine, car.engine);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(year, color, wheels, engine);
+    }
+
+
+    public Engine getEngine() {
+        return engine == null ? null : engine.clone();
+    }
+
+    public List<Wheel> getWheels() {
+        return createClonedListOfWheels(wheels);
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Car changeEngine(Engine newEngine) {
+        return new Car(year, color, wheels, newEngine);
+    }
+
+    public Car addWheel(Wheel newWheel) {
+        List<Wheel> newWheels = new ArrayList<>(wheels);
+        newWheels.add(newWheel.clone());
+        return new Car(year, color, newWheels, engine);
+    }
+
+    public Car changeColor(String newColor) {
+        return new Car(year, newColor, wheels, engine);
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    private List<Wheel> createClonedListOfWheels(List<Wheel> wheels) {
+        List<Wheel> clonedList = new ArrayList<>();
+        for (Wheel wheel : wheels) {
+            clonedList.add(wheel.clone());
+        }
+        return clonedList;
+    }
 }
