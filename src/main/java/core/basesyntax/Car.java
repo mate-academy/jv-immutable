d581bf004809f7736fd26c5c1b6962cef00eaff4
egorka <entertainment.veks@gmail.com>
2020-12-21 11:50:00 +0200

    getters changed to local variables

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -71,7 +71,7 @@ public final class Car {
             return false;
         }
         Car car = (Car) o;
-        return getYear() == car.getYear()
+        return year == car.year
                 && Objects.equals(color, car.color)
                 && Objects.equals(wheels, car.wheels)
                 && Objects.equals(engine, car.engine);
