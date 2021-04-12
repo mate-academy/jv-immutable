0aa6c990b69478b636a206025ded9d42cbbc767b
Nikber5 <nikber198@gmail.com>
2021-04-12 15:09:41 +0300

    Done

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+//To make one more pull request
 public final class Car {
     private final int year;
     private final String color;
