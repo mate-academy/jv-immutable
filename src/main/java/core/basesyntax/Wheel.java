70c235562f24740ba733e85b7066f23104e14725
vuyko23 <mykolasakulych@gmail.com>
2021-04-14 20:45:22 +0300

    immutable solution

--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,5 +1,46 @@
 package core.basesyntax;
 
-public class Wheel {
+public class Wheel implements Cloneable {
     private int radius;
+
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    @Override
+    protected Wheel clone() {
+        try {
+            return (Wheel) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can`t correctly copy Wheel", e);
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return 17 * 13 + radius;
+    }
+
+    @Override
+    public boolean equals(Object wheel) {
+        if (wheel == this) {
+            return true;
+        }
+        if (wheel == null) {
+            return false;
+        }
+        if (wheel.getClass().equals(Wheel.class)) {
+            Wheel currentWheel = (Wheel) wheel;
+            return radius == currentWheel.radius;
+        }
+        return false;
+    }
 }
