a4f8262e4a26b93faa330eaf9859fea663107ace
Cyouffy <cyouffy@gmail.com>
2021-04-19 18:24:24 +0300

    added solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,13 +1,93 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Make this class immutable. See requirements in the README file
  */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
+
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = cloneWheels(wheels);
+        if (engine == null) {
+            this.engine = null;
+        } else {
+            this.engine = engine.clone();
+        }
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        return cloneWheels(wheels);
+    }
+
+    public Engine getEngine() {
+        if (engine == null) {
+            return null;
+        } else {
+            return engine.clone();
+        }
+    }
+
+    public List<Wheel> cloneWheels(List<Wheel> wheels) {
+        List<Wheel> newListWheels = new ArrayList<>();
+        for (Wheel wheel : wheels) {
+            newListWheels.add(wheel.clone());
+        }
+        return newListWheels;
+    }
+
+    public Car addWheel(Wheel newWheel) {
+        List<Wheel> clonedLisWheel = cloneWheels(this.wheels);
+        clonedLisWheel.add(newWheel);
+        return new Car(year, color, clonedLisWheel, engine);
+    }
+
+    public Car changeEngine(Engine newEngine) {
+        return new Car(year, color, wheels, newEngine);
+    }
+
+    public Car changeColor(String newColor) {
+        return new Car(year, newColor, wheels, engine);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = year;
+        hash = 17 * hash + (color != null ? color.hashCode() : 0);
+        hash = 17 * hash + (wheels != null ? wheels.hashCode() : 0);
+        hash = 17 * hash + (engine != null ? engine.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Car car = (Car) obj;
+        if (year != car.year || !Objects.equals(color, car.color)
+                || !Objects.equals(wheels, car.wheels)) {
+            return false;
+        }
+        return Objects.equals(engine, car.engine);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,6 +1,63 @@
 package core.basesyntax;
 
-public class Engine {
+import java.util.Objects;
+
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
+
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = horsePower;
+        hash = 17 * hash + ((manufacturer != null ? manufacturer.hashCode() : 0));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Engine engine = (Engine) obj;
+
+        if (engine.getHorsePower() != horsePower) {
+            return false;
+        }
+
+        return Objects.equals(manufacturer, engine.manufacturer);
+    }
+
+    @Override
+    protected Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't create Engine clone", e);
+        }
+    }
 }
