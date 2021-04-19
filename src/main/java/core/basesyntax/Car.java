1e99c385557ba460fd4adc7b953b820470a51085
Stanislav Aleksandrovych <astanislav2361@gmail.com>
2021-04-19 18:15:06 +0300

    Task was completed

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,13 +1,92 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Make this class immutable. See requirements in the README file
  */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
+
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = cloneListOfWheels(wheels);
+        this.engine = cloneEngine(engine);
+    }
+
+    public Car changeEngine(Engine engine) {
+        return new Car(year, color, wheels, engine);
+    }
+
+    public Car changeColor(String newColor) {
+        return new Car(year, newColor, wheels, engine);
+    }
+
+    public Car addWheel(Wheel newWheel) {
+        List<Wheel> wheelList = new ArrayList<>(wheels);
+        wheelList.add(newWheel);
+        return new Car(year, color, wheelList, engine);
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        return new ArrayList<>(wheels);
+    }
+
+    public Engine getEngine() {
+        return engine == null ? null : engine.clone();
+    }
+
+    @Override
+    public boolean equals(Object car) {
+        if (this == car) {
+            return true;
+        }
+        if (car == null) {
+            return false;
+        }
+        if (car.getClass().equals(Car.class)) {
+            Car current = (Car) car;
+            return Objects.equals(this.engine, current.engine)
+                    && Objects.equals(this.wheels, current.wheels)
+                    && Objects.equals(this.color, current.color)
+                    && this.year == current.year;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + (engine == null ? 0 : engine.hashCode());
+        result = 31 * result + (wheels == null ? 0 : wheels.hashCode());
+        result = 31 * result + year;
+        result = 31 * result + (color == null ? 0 : color.hashCode());
+        return result;
+    }
+
+    private List<Wheel> cloneListOfWheels(List<Wheel> listOfWheels) {
+        List<Wheel> newList = new ArrayList<>();
+        for (Wheel wheel : listOfWheels) {
+            newList.add(wheel.clone());
+        }
+        return newList;
+    }
+
+    private Engine cloneEngine(Engine engine) {
+        return engine == null ? null : engine.clone();
+    }
 }
--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,5 +1,48 @@
 package core.basesyntax;
 
-public class Wheel {
+public class Wheel implements Cloneable {
     private int radius;
+
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public boolean equals(Object wheel) {
+        if (this == wheel) {
+            return true;
+        }
+        if (wheel == null) {
+            return false;
+        }
+        if (wheel.getClass().equals(Wheel.class)) {
+            Wheel current = (Wheel) wheel;
+            return this.radius == current.radius;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + radius;
+        return result;
+    }
+
+    @Override
+    public Wheel clone() {
+        try {
+            return (Wheel) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't create clone for Wheel object", e);
+        }
+    }
 }
