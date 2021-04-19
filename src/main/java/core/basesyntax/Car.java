1d38ca69004c72066d62a7c09fed3c292f06ba11
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-04-19 14:38:09 +0300

    First version.

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,13 +1,136 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Make this class immutable. See requirements in the README file
  */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car implements Cloneable {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private  Engine engine;
+
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = new ArrayList<>(wheels);
+        if (engine != null) {
+            this.engine = engine.clone();
+        }
+    }
+
+    private Car(Builder builder) {
+        this.year = builder.year;
+        this.color = builder.color;
+        this.wheels = new ArrayList<>(builder.wheels);
+        this.engine = builder.engine.clone();
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        return new ArrayList<>(wheels);
+    }
+
+    public Engine getEngine() {
+        return engine != null ? engine.clone() : null;
+    }
+
+    public Car addWheel(Wheel newWheel) {
+        Car carClone = this.clone();
+        List<Wheel> wheelsActual = carClone.getWheels();
+        wheelsActual.add(newWheel);
+        return new Builder().setYear(carClone.year)
+                            .setColor(carClone.color)
+                            .setWheels(wheelsActual)
+                            .setEngine(carClone.engine)
+                            .build();
+    }
+
+    public Car changeColor(String newColor) {
+        Car carClone = this.clone();
+        return new Builder().setYear(carClone.year)
+                            .setColor(newColor)
+                            .setWheels(carClone.wheels)
+                            .setEngine(carClone.engine)
+                            .build();
+    }
+
+    public Car changeEngine(Engine engine) {
+        Car carClone = this.clone();
+        return new Builder().setYear(carClone.year)
+                            .setColor(carClone.color)
+                            .setWheels(carClone.wheels)
+                            .setEngine(engine)
+                            .build();
+    }
+
+    @Override
+    protected Car clone() {
+        try {
+            return (Car) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can`t create clone for Car object", e);
+        }
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        Car car = (Car) object;
+        return year == car.year
+                && Objects.equals(color, car.color)
+                && Objects.equals(wheels, car.wheels)
+                && Objects.equals(engine, car.engine);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(year, color, wheels, engine);
+    }
+
+    private static class Builder {
+        private int year;
+        private String color;
+        private List<Wheel> wheels;
+        private Engine engine;
+
+        public Builder setYear(int year) {
+            this.year = year;
+            return this;
+        }
+
+        public Builder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public Builder setWheels(List<Wheel> wheels) {
+            this.wheels = wheels;
+            return this;
+        }
+
+        public Builder setEngine(Engine engine) {
+            this.engine = engine;
+            return this;
+        }
+
+        public Car build() {
+            return new Car(this);
+        }
+    }
 }
--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,5 +1,43 @@
 package core.basesyntax;
 
-public class Wheel {
+public class Wheel implements Cloneable {
     private int radius;
+
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        Wheel wheel = (Wheel) object;
+        return radius == wheel.radius;
+    }
+
+    @Override
+    public int hashCode() {
+        return getRadius();
+    }
+
+    @Override
+    public Wheel clone() {
+        try {
+            return (Wheel) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can`t create clone for Engine object", e);
+        }
+    }
 }
