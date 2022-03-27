4ebfbcc60e886645b260d8d8a72ae0be0a57193b
Mariana Trinko <marjanavv@gmail.com>
2022-03-27 18:40:54 +0200

    Immutable

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -2,14 +2,15 @@ package core.basesyntax;
 
 import java.util.Objects;
 
-public class Engine implements Cloneable{
-    private  int horsePower;
-    private   String manufacturer;
+public class Engine implements Cloneable {
+    private int horsePower;
+    private String manufacturer;
 
     public Engine(int horsePower, String manufacturer) {
         this.horsePower = horsePower;
         this.manufacturer = manufacturer;
     }
+
     public int getHorsePower() {
         return horsePower;
     }
@@ -17,39 +18,48 @@ public class Engine implements Cloneable{
     public String getManufacturer() {
         return manufacturer;
     }
+
     public void setManufacturer(String manufacturer) {
         this.manufacturer = manufacturer;
     }
+
     public void setHorsePower(int horsePower) {
         this.horsePower = horsePower;
     }
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Engine)) return false;
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Engine)) {
+            return false;
+        }
         Engine engine = (Engine) o;
-        return getHorsePower() == engine.getHorsePower() && Objects.equals(getManufacturer(), engine.getManufacturer());
+        return getHorsePower() == engine.getHorsePower()
+                && Objects.equals(getManufacturer(), engine.getManufacturer());
     }
+
     @Override
     public int hashCode() {
         return Objects.hash(getHorsePower(), getManufacturer());
     }
 
     @Override
-    public Engine clone()  {
-        try{
+    public Engine clone() {
+        try {
             Engine engineCopy = (Engine) super.clone();
             return engineCopy;
-        }catch(CloneNotSupportedException e){
-          throw new RuntimeException("Can´t clone engine", e)  ;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can´t clone engine", e);
         }
     }
 
     @Override
     public String toString() {
         return "Engine{"
-            + "horsePower=" + horsePower
-            + ", manufacturer='" + manufacturer + '\''
-            + '}';
+                + "horsePower=" + horsePower
+                + ", manufacturer='" + manufacturer + '\''
+                + '}';
     }
 }
