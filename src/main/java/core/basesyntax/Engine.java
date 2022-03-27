297decfcd4ca2819ef682c3e26eb2fb243015f79
Mariana Trinko <marjanavv@gmail.com>
2022-03-27 18:25:07 +0200

    Immutable

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,10 +1,49 @@
 package core.basesyntax;
 
-public class Engine {
-    private int horsePower;
-    private String manufacturer;
+import java.util.Objects;
 
-    //implement this class
+public class Engine implements Cloneable{
+    private  int horsePower;
+    private   String manufacturer;
+
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Engine)) return false;
+        Engine engine = (Engine) o;
+        return getHorsePower() == engine.getHorsePower() && Objects.equals(getManufacturer(), engine.getManufacturer());
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(getHorsePower(), getManufacturer());
+    }
+
+    @Override
+    public Engine clone()  {
+        try{
+            Engine engineCopy = (Engine) super.clone();
+            return engineCopy;
+        }catch(CloneNotSupportedException e){
+          throw new RuntimeException("Can´t clone engine", e)  ;
+        }
+    }
 
     @Override
     public String toString() {
