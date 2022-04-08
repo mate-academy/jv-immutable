b3b2c7fc08d77b6bbb06d6248f15906bb37f0c89
Denys.Sh <7860denis@gmail.com>
2022-04-08 23:26:31 +0300

    my solution fail 1 test NPE

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,25 +1,110 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Make this class immutable. See requirements in task description.
- */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
 
-    //implement this class
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = getCopyWheels(wheels);
+        this.engine = engine != null ? engine.clone() : null;
+    }
+
+    public Car changeEngine(Engine newEngine) {
+        if (newEngine == null) {
+            throw new NullPointerException("Engine can`t be null");
+        }
+        return new Car(year, color, wheels, newEngine);
+    }
+
+    public Car changeColor(String newColor) {
+        return new Car(year, newColor, wheels, engine);
+    }
+
+    public Car addWheel(Wheel wheel) {
+        if (wheel == null) {
+            throw new NullPointerException("Wheel can`t be null");
+        }
+        List<Wheel> newWheels = getWheels();
+        newWheels.add(wheel);
+        return new Car(year, color, newWheels, engine);
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        if (wheels == null) {
+            throw new NullPointerException("Wheels can`t be null");
+        }
+        return getCopyWheels(wheels);
+    }
+
+    public Engine getEngine() {
+        if (engine == null) {
+            throw new NullPointerException("Engine can`t be null");
+        }
+        return engine.clone();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Car car = (Car) o;
+
+        if (year != car.year) {
+            return false;
+        }
+        if (color != null ? !color.equals(car.color) : car.color != null) {
+            return false;
+        }
+        if (wheels != null ? !wheels.equals(car.wheels) : car.wheels != null) {
+            return false;
+        }
+        return engine != null ? engine.equals(car.engine) : car.engine == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = year;
+        result = 31 * result + (color != null ? color.hashCode() : 0);
+        result = 31 * result + (wheels != null ? wheels.hashCode() : 0);
+        result = 31 * result + (engine != null ? engine.hashCode() : 0);
+        return result;
+    }
 
     @Override
     public String toString() {
         return "Car{"
-            + "year=" + year
-            + ", color='" + color + '\''
-            + ", wheels=" + wheels
-            + ", engine=" + engine
-            + '}';
+                + "year=" + year
+                + ", color='" + color + '\''
+                + ", wheels=" + wheels
+                + ", engine=" + engine
+                + '}';
+    }
+
+    private List<Wheel> getCopyWheels(List<Wheel> wheelList){
+        List<Wheel> copyWheels = new ArrayList<>(wheelList.size());
+        for (Wheel wheel : wheelList) {
+            copyWheels.add(wheel.clone());
+        }
+        return copyWheels;
     }
 }
