3e70ddf09d882411763e1a800048dde4192cfbf5
Andrii Synelnyk <sinelnik2154@gmail.com>
2022-04-15 19:20:55 +0300

    class Сar was made immutable

--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,14 +1,53 @@
 package core.basesyntax;
 
-public class Wheel {
+public class Wheel implements Cloneable {
     private int radius;
 
-    //implement this class
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
 
     @Override
     public String toString() {
         return "Wheel{"
-            + "radius=" + radius
-            + '}';
+                + "radius=" + radius
+                + '}';
+    }
+
+    @Override
+    public Wheel clone() {
+        return new Wheel(radius);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 23;
+        result = 31 * result + radius;
+        return result;
+    }
+
+    public boolean equals(Object object) {
+        if (object == null) {
+            return false;
+        }
+
+        if (object == this) {
+            return true;
+        }
+
+        if (object.getClass() == Wheel.class) {
+            Wheel wheel = (Wheel) object;
+            return this.radius == wheel.radius;
+        }
+
+        return false;
     }
 }
