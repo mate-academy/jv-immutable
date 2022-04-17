fadc35dc81f9c7cb011fd36ef91c12c68d97d1ea
Gigawatt94 <101740223+Gigawatt94@users.noreply.github.com>
2022-04-17 22:24:32 +0300

    changing Car class

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -5,13 +5,36 @@ import java.util.List;
 /**
  * Make this class immutable. See requirements in task description.
  */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car implements Cloneable {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
 
-    //implement this class
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        return wheels;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    @Override
+    protected Car clone() {
+        try {
+            return (Car) super.clone();
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Object can't be cloned");
+        }
+    }
 
     @Override
     public String toString() {
