9d9cdb8e61b84d0a84b2dcb12b13f47dae460893
Viktoriia Ihnatiuk <victoriia.ihnatiuk@gmail.com>
2022-04-18 06:40:00 +0300

    Immutable homework solution added

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,10 +1,60 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
-    //implement this class
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Engine engine = (Engine) o;
+        return ((horsePower == engine.horsePower)
+                && (manufacturer == manufacturer || manufacturer != null
+                && manufacturer.equals(engine.manufacturer)));
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = result * 31 + horsePower;
+        result = result * 31 + (manufacturer == null ? null : manufacturer.hashCode());
+        return result;
+    }
+
+    @Override
+    protected Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can not clone Engine object", e);
+        }
+    }
 
     @Override
     public String toString() {
