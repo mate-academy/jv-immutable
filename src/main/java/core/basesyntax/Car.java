2b7e74f85d63794ef4d79d886a8c3ccfef2d6a02
Denys Skrinnik <skrinnikden2000@gmail.com>
2022-05-13 21:40:57 +0300

    done

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,17 +1,55 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-/**
- * Make this class immutable. See requirements in task description.
- */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car implements Cloneable {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
 
-    //implement this class
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        List<Wheel> listWheel = copyWheels(wheels);
+        this.year = year;
+        this.color = color;
+        this.wheels = listWheel;
+        this.engine = engine == null ? null : engine.clone();
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        List<Wheel> wheels = copyWheels(this.wheels);
+        return wheels;
+    }
+
+    public Engine getEngine() {
+        return engine == null ? null : engine.clone();
+    }
+
+    public Car changeEngine(Engine engine) {
+        List<Wheel> listWheel = copyWheels(this.wheels);
+        return new Car(this.year, this.color, listWheel, engine);
+    }
+
+    public Car changeColor(String newColor) {
+        List<Wheel> listWheel = copyWheels(this.wheels);
+        return new Car(this.year, newColor, listWheel, this.engine.clone());
+    }
+
+    public Car addWheel(Wheel newWheel) {
+        List<Wheel> listWheel = copyWheels(this.wheels);
+        listWheel.add(newWheel);
+        return new Car(this.year, this.color, listWheel, this.engine.clone());
+    }
 
     @Override
     public String toString() {
@@ -21,5 +59,31 @@ public class Car {
             + ", wheels=" + wheels
             + ", engine=" + engine
             + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return year == car.year && Objects.equals(color, car.color)
+                && Objects.equals(wheels, car.wheels) && Objects.equals(engine, car.engine);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(year, color, wheels, engine);
+    }
+
+    private List<Wheel> copyWheels(List<Wheel> wheels) {
+        List<Wheel> list = new ArrayList<>();
+        for (Wheel wheel : wheels) {
+            list.add(wheel.clone());
+        }
+        return list;
     }
 }
