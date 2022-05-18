f6275221686d8ae235ea62a9f0d6a3e500ab2cdf
Maksym <Maksym_Personal@ip-192-168-0-71.eu-central-1.compute.internal>
2022-05-18 19:54:21 +0300

    22/30

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,17 +1,47 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
  * Make this class immutable. See requirements in task description.
  */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
+
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        List<Wheel> wheelsCopy = new ArrayList<>();
+        for (Wheel wheel : wheels) {
+            wheelsCopy.add(wheel.clone());
+        }
+        this.year = year;
+        this.color = color;
+        this.wheels = wheelsCopy;
+        this.engine = engine.clone();
+    }
 
     //implement this class
+
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        return wheels;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
 
     @Override
     public String toString() {
@@ -21,5 +51,20 @@ public class Car {
             + ", wheels=" + wheels
             + ", engine=" + engine
             + '}';
+    }
+
+    public Car changeEngine(Engine engine) {
+        this.engine.setHorsePower(engine.getHorsePower());
+        this.engine.setManufacturer(engine.getManufacturer());
+        return this;
+    }
+
+    public Car changeColor(String newColor) {
+        return new Car(this.year, newColor, this.wheels, this.engine);
+    }
+
+    public Car addWheel(Wheel newWheel) {
+        wheels.add(newWheel.clone());
+        return this;
     }
 }
