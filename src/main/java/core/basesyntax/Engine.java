4482c2e5705a098855e45695c616fa2146dffa00
Tymur Tsvietkov <tymur.tsvietkov@gmail.com>
2022-06-06 22:15:05 +0300

    hw-immutable-class

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,16 +1,67 @@
 package core.basesyntax;
 
-public class Engine {
+import java.util.Objects;
+
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
-    //implement this class
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public Object getManufacturer() {
+        return manufacturer;
+    }
+
+    public Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't clone Engine", e);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Engine)) {
+            return false;
+        }
+        Engine engine = (Engine) o;
+        if (horsePower != engine.horsePower) {
+            return false;
+        }
+        return Objects.equals(manufacturer, engine.manufacturer);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = horsePower;
+        result = 31 * result + (manufacturer != null ? manufacturer.hashCode() : 0);
+        return result;
+    }
 
     @Override
     public String toString() {
         return "Engine{"
-            + "horsePower=" + horsePower
-            + ", manufacturer='" + manufacturer + '\''
-            + '}';
+                + "horsePower=" + horsePower
+                + ", manufacturer='" + manufacturer + '\''
+                + '}';
     }
 }
