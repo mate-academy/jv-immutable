2bf4d9eeefff3a08b9e1a23c746999f1cebc1a95
Oleksii Kordysh <aleksey.kordysh@gmail.com>
2022-06-06 16:12:25 +0300

    immutable for class Car implemented

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,10 +1,29 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
-    //implement this class
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
 
     @Override
     public String toString() {
@@ -12,5 +31,40 @@ public class Engine {
             + "horsePower=" + horsePower
             + ", manufacturer='" + manufacturer + '\''
             + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Engine engine = (Engine) o;
+
+        if (getHorsePower() != engine.getHorsePower()) {
+            return false;
+        }
+        return getManufacturer() != null
+                ? getManufacturer().equals(engine.getManufacturer())
+                : engine.getManufacturer() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getHorsePower();
+        result = 31 * result + (getManufacturer() != null ? getManufacturer().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError("Can't create clone for Engine :", e);
+        }
     }
 }
