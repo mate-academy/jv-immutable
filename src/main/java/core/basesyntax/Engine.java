6deba9ef7b8b3827ba41eeec701407cf7ff8aaef
Yaroslav Lastivka <yaroslav0108@gmail.com>
2022-06-06 23:35:44 +0300

    Immutable class Car was created

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,10 +1,60 @@
 package core.basesyntax;
 
-public class Engine {
+import java.util.Objects;
+
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
-    //implement this class
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + horsePower;
+        result = 31 * result + (manufacturer == null ? 0 : manufacturer.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+        Engine engine = (Engine) obj;
+        return horsePower == engine.horsePower
+                && Objects.equals(this.manufacturer, engine.manufacturer);
+    }
+
+    @Override
+    public Engine clone() {
+        return new Engine(horsePower, manufacturer);
+    }
 
     @Override
     public String toString() {
