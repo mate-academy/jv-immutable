0783cc8f307667ddedf86e750dc09e00f5cfa5dd
Dmytro Lemeshchenko <dimmonishe@gmail.com>
2022-06-13 20:12:55 +0300

    Let's go!

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,17 +1,78 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Make this class immutable. See requirements in task description.
  */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car implements Cloneable {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
 
-    //implement this class
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = getWheels(wheels);
+        this.engine = engine == null ? null : engine.clone();
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        return getWheels(wheels);
+    }
+
+    private List<Wheel> getWheels(List<Wheel> wheels) {
+        List<Wheel> wheelsCopy = new ArrayList<>(wheels.size());
+        for (Wheel wheel : wheels) {
+            wheelsCopy.add(wheel.clone());
+        }
+        return wheelsCopy;
+    }
+
+    public Engine getEngine() {
+        if (engine == null) {
+            return null;
+        }
+        return engine.clone();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return year == car.year && Objects.equals(color, car.color)
+                && Objects.equals(wheels, car.wheels) && Objects.equals(engine, car.engine);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(year, color, wheels, engine);
+    }
+
+    @Override
+    public Car clone() {
+        try {
+            return (Car) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't create clone of Car object!", e);
+        }
+    }
 
     @Override
     public String toString() {
@@ -22,4 +83,24 @@ public class Car {
             + ", engine=" + engine
             + '}';
     }
+
+    public Car addWheel(Wheel newWheel) {
+        List<Wheel> list = getWheels(wheels);
+        list.add(newWheel);
+        Car cloneCar = new Car(year, color, getWheels(list), engine);
+
+        return cloneCar;
+    }
+
+    public Car changeEngine(Engine engine) {
+        Engine newEngine = engine.clone();
+        Car cloneCar = new Car(year, color, wheels, newEngine);
+        return cloneCar;
+    }
+
+    public Car changeColor(String newColor) {
+        Car cloneCar = new Car(year, newColor, wheels, engine);
+        return cloneCar;
+    }
+
 }
