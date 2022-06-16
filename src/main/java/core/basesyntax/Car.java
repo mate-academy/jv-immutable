5c4a7fd23efcdec6bbbd03440823483982f36897
Sergii Turchyn <turchyn13@gmail.com>
2022-06-16 13:13:25 +0300

    implemented car

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,17 +1,53 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Make this class immutable. See requirements in task description.
  */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
+public final class Car {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
     private Engine engine;
 
-    //implement this class
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = getClonedWheels(wheels);
+        this.engine = engine.clone();
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        return getClonedWheels(wheels);
+    }
+
+    public Engine getEngine() {
+        return engine.clone();
+    }
+
+    public Car changeEngine(Engine engine) {
+        return new Car(year, color, wheels, engine.clone());
+    }
+
+    public Car changeColor(String newColor) {
+        return new Car(year, newColor, wheels, engine);
+    }
+
+    public Car addWheel(Wheel newWheel) {
+        return new Car(year, color, wheels, engine.clone());
+    }
+
+
 
     @Override
     public String toString() {
@@ -21,5 +57,13 @@ public class Car {
             + ", wheels=" + wheels
             + ", engine=" + engine
             + '}';
+    }
+
+    private List<Wheel> getClonedWheels(List<Wheel> wheels) {
+        List<Wheel> destanation = new ArrayList<>(wheels.size());
+        for (Wheel wheel : wheels) {
+            destanation.add(wheel.clone());
+        }
+        return destanation;
     }
 }
