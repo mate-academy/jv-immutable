c606e10f80ff1aee44e670a21b03bba811f6dd2c
Ilya <zimmboco@gmail.com>
2022-06-17 11:34:55 +0300

    hw-14

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public final class Car implements Cloneable{
+public final class Car implements Cloneable {
     private final int year;
     private final String color;
     private final List<Wheel> wheels;
