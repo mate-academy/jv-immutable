b9729034088a4421e0698910b920b9ce59707ae4
Serhii Buria <sergeiburya@gmail.com>
2022-07-26 20:43:17 +0300

    added implementation of the car class

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,6 +1,8 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public final class Car {
     private final int year;
@@ -11,8 +13,8 @@ public final class Car {
     public Car(int year, String color, List<Wheel> wheels, Engine engine) {
         this.year = year;
         this.color = color;
-        this.wheels = wheels;
-        this.engine = engine;
+        this.wheels = cloneWheels(wheels);
+        this.engine = сloneEngine(engine);
     }
 
     public int getYear() {
@@ -24,12 +26,48 @@ public final class Car {
     }
 
     public List<Wheel> getWheels() {
-        return wheels;
+        return cloneWheels(wheels);
     }
 
     public Engine getEngine() {
-        return engine;
+        return сloneEngine(engine);
     }
+
+    public Car changeEngine(Engine engine) {
+        return new Car(year, color, wheels, engine);
+    }
+
+    public Car changeColor(String newColor) {
+        return new Car(year, newColor, wheels, engine);
+    }
+
+    public Car addWheel(Wheel wheel) {
+        List<Wheel> newWheel = new ArrayList(wheels);
+        newWheel.add(wheel);
+        return new Car(year, color, newWheel, engine);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Car)) {
+            return false;
+        }
+        Car car = (Car) obj;
+        return getYear() == car.getYear()
+                && Objects.equals(color, car.color)
+                && Objects.equals(getWheels(), car.getWheels())
+                && Objects.equals(getEngine(), car.getEngine());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getYear(), color, getWheels(), getEngine());
+    }
+
+
 
     @Override
     public String toString() {
@@ -40,4 +78,17 @@ public final class Car {
                 + ", engine=" + engine
                 + '}';
     }
+
+    private List<Wheel> cloneWheels(List<Wheel> copyFromList) {
+        List<Wheel> wheelList = new ArrayList<>();
+        for (Wheel wheel : copyFromList) {
+            wheelList.add(wheel.clone());
+        }
+        return wheelList;
+    }
+
+    private Engine сloneEngine(Engine engine) {
+        return engine == null ? null : engine.clone();
+    }
+
 }
