c7941131a60b9f187b8b102ee21f2b339b37d54e
Serhii Buria <sergeiburya@gmail.com>
2022-07-26 19:58:53 +0300

    added implementation of the engine classÑÑ

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,16 +1,65 @@
 package core.basesyntax;
 
-public class Engine {
+import java.util.Objects;
+
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
-    //implement this class
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
 
     @Override
     public String toString() {
         return "Engine{"
-            + "horsePower=" + horsePower
-            + ", manufacturer='" + manufacturer + '\''
-            + '}';
+                + "horsePower=" + horsePower
+                + ", manufacturer='" + manufacturer + '\''
+                + '}';
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Engine)) {
+            return false;
+        }
+        Engine engine = (Engine) obj;
+        return getHorsePower() == engine.getHorsePower()
+                && Objects.equals(getManufacturer(), engine.getManufacturer());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getHorsePower(), getManufacturer());
+    }
+
+    @Override
+    public Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't create clone of Engine", e);
+        }
+    }
+
 }
