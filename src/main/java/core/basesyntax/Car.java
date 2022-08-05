e17283495da36f96389b158aed0d45e5d9ffa93e
Andrii Pylypenko <pilipenko1975@gmail.com>
2022-08-05 23:58:04 +0300

    task completed

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,25 +1,95 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-/**
- * Make this class immutable. See requirements in task description.
- */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
 
-    //implement this class
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        if (wheels != null) {
+            this.year = year;
+            this.color = color;
+            this.wheels = getWheels(wheels);
+            if (engine != null) {
+                this.engine = new Engine(engine.getHorsePower(), engine.getManufacturer());
+            } else {
+                this.engine = null;
+            }
+        } else {
+            throw new NullPointerException();
+        }
+    }
+
+    public Car addWheel(Wheel newWheel) {
+        List<Wheel> newWheels = getWheels(wheels);
+        newWheels.add(new Wheel(newWheel.getRadius()));
+        return new Car(getYear(), getColor(), newWheels,
+                Objects.requireNonNull(getEngine()).clone());
+    }
+
+    public Car changeEngine(Engine engine) {
+        return new Car(getYear(), getColor(), getWheels(), engine.clone());
+    }
+
+    public Car changeColor(String color) {
+        return new Car(getYear(), color, getWheels(), getEngine());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return getYear() == car.getYear() && Objects.equals(getColor(), car.getColor())
+                && getWheels().equals(car.getWheels())
+                && Objects.equals(getEngine(), car.getEngine());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getYear(), getColor(), getWheels(), getEngine());
+    }
 
     @Override
     public String toString() {
         return "Car{"
-            + "year=" + year
-            + ", color='" + color + '\''
-            + ", wheels=" + wheels
-            + ", engine=" + engine
-            + '}';
+                + "year=" + year
+                + ", color='" + color + '\''
+                + ", wheels=" + wheels
+                + ", engine=" + engine
+                + '}';
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        List<Wheel> wheelList = new ArrayList<>();
+        for (Wheel currentWheel : wheels) {
+            wheelList.add(currentWheel.clone());
+        }
+        return wheelList;
+    }
+
+    public List<Wheel> getWheels(List<Wheel> wheels) {
+        List<Wheel> wheelList = new ArrayList<>();
+        for (Wheel currentWheel : wheels) {
+            wheelList.add(currentWheel.clone());
+        }
+        return wheelList;
+    }
+
+    public Engine getEngine() {
+        return engine == null ? null : engine.clone();
     }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,16 +1,61 @@
 package core.basesyntax;
 
-public class Engine {
+import java.util.Objects;
+
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
-    //implement this class
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
 
     @Override
     public String toString() {
         return "Engine{"
-            + "horsePower=" + horsePower
-            + ", manufacturer='" + manufacturer + '\''
-            + '}';
+                + "horsePower=" + horsePower
+                + ", manufacturer='" + manufacturer + '\''
+                + '}';
+    }
+
+    @Override
+    public Engine clone() {
+        Engine cloneEngine;
+        try {
+            cloneEngine = (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+        return cloneEngine;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Engine engine = (Engine) object;
+        return getHorsePower() == engine.getHorsePower() && getManufacturer().equals(engine.getManufacturer());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getHorsePower(), getManufacturer());
     }
 }
