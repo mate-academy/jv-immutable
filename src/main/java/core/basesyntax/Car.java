9f7630c809e777c7e7fe7bb41fc52583f15a5c23
LubusUzh <lubus.uzh@gmail.com>
2022-08-17 13:20:49 +0300

    Add: in class Car add final keyword to the name of class and to the values, add Car constructor; in class Wheel implement Cloneablem, add constructor, getter, setter, override hashCode, equals and clone methods

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -5,13 +5,18 @@ import java.util.List;
 /**
  * Make this class immutable. See requirements in task description.
  */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
 
-    //implement this class
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = wheels;
+        this.engine = engine;
+    }
 
     @Override
     public String toString() {
--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,9 +1,45 @@
 package core.basesyntax;
 
-public class Wheel {
+public class Wheel implements Cloneable {
     private int radius;
 
-    //implement this class
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public int hashCode() {
+        return Object.hashCode(getRadius());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Wheel)) {
+            return false;
+        }
+        Wheel wheel = (Wheel) obj;
+        return getRadius() == wheel.getRadius();
+    }
+
+    @Override
+    protected Object clone()  {
+        try {
+            return (Wheel) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't create wheel clone", e);
+        }
+    }
 
     @Override
     public String toString() {
