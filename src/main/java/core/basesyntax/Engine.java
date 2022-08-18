f46127a366deae949a57e22eff21195d7c3cb05a
mr-Claster <67782747+mr-Claster@users.noreply.github.com>
2022-08-18 15:33:32 +0300

    first version done

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,10 +1,63 @@
 package core.basesyntax;
 
-public class Engine {
+import java.util.Objects;
+
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
-    //implement this class
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    @Override
+    public Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (obj.getClass() == Engine.class) {
+            return Objects.equals(((Engine) obj).manufacturer, manufacturer)
+                    && horsePower == horsePower;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int number = 31;
+        number = manufacturer == null ? 0 : number * 17 + manufacturer.hashCode();
+        number = number * 17 + horsePower;
+        return number;
+    }
 
     @Override
     public String toString() {
