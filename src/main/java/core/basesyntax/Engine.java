9eaefa11b9447ff9834ebda60e5befee1e5210ad
Andriy Linkov <linkoff.as@gmail.com>
2022-08-19 00:06:42 +0300

    Implemented classes

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,10 +1,61 @@
 package core.basesyntax;
 
-public class Engine {
+import java.util.Objects;
+
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
-    //implement this class
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    public boolean equals(Object engine) {
+        if (this == engine) {
+            return true;
+        }
+        if (engine == null) {
+            return false;
+        }
+        if (engine.getClass().equals(Engine.class)) {
+            Engine current = (Engine) engine;
+            return (this.horsePower == current.horsePower
+                    && (Objects.equals(this.manufacturer, current.manufacturer)));
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(horsePower, manufacturer);
+    }
+
+    @Override
+    protected Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Cann't create clone of Engine", e);
+        }
+    }
 
     @Override
     public String toString() {
