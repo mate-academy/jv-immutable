1b42f030a8989338a87d63c789a324f2652d6745
galenkoigor <galenkoigor@gmail.com>
2022-10-12 23:11:34 +0200

    initial commit

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -6,12 +6,33 @@ import java.util.List;
  * Make this class immutable. See requirements in task description.
  */
 public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
 
-    //implement this class
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = wheels;
+        this.engine = engine;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        return wheels;
+    }
+
+    public Engine getEngine() {
+        return engine.clone();
+    }
 
     @Override
     public String toString() {
@@ -21,5 +42,17 @@ public class Car {
             + ", wheels=" + wheels
             + ", engine=" + engine
             + '}';
+    }
+
+    public Car changeEngine(Engine other_maker) {
+        return null;
+    }
+
+    public Car addWheel(Wheel wheel) {
+        return null;
+    }
+
+    public Car changeColor(String blue) {
+        return null;
     }
 }
