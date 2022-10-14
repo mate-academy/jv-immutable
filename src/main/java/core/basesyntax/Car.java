b825bfeb9c375ac8a9b4cec8a43374ec2ef29887
Dima Senko <d.senko@i.ua>
2022-10-14 10:47:27 +0300

    Commit

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,17 +1,31 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-/**
- * Make this class immutable. See requirements in task description.
- */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car {
+    final private int year;
+    final private String color;
+    final private List<Wheel> wheels;
+    final private Engine engine;
 
-    //implement this class
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = getCloneWheels(wheels);
+        this.engine = getCloneEngine(engine);
+    }
+
+    public Car addWheel(Wheel newWheel) {
+        List<Wheel> newWheelClone = getCloneWheels(wheels);
+        newWheelClone.add(wheels.size(), newWheel);
+        return new Car(year, color, newWheelClone, engine);
+    }
+
+    public Car changeColor(String newColor) {
+        return new Car(year, newColor, wheels, engine);
+    }
 
     @Override
     public String toString() {
@@ -21,5 +35,68 @@ public class Car {
             + ", wheels=" + wheels
             + ", engine=" + engine
             + '}';
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        return getCloneWheels(wheels);
+    }
+
+    public Engine getEngine() {
+        return getCloneEngine(engine);
+    }
+
+    public Car changeEngine(Engine engine) {
+        return new Car(year, color, wheels, engine);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Car)) {
+            return false;
+        }
+        Car car = (Car) o;
+        return year == car.year && Objects.equals(color, car.color)
+                && Objects.equals(wheels, car.wheels)
+                && Objects.equals(engine, car.engine);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(year, color, wheels, engine);
+    }
+
+    @Override
+    public Car clone() {
+        try {
+            return (Car) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Engine getCloneEngine(Engine engine) {
+        if (engine == null) {
+            return null;
+        }
+        return new Engine(engine.getHorsePower(), engine.getManufacturer());
+    }
+
+    private List<Wheel> getCloneWheels(List<Wheel> wheels) {
+        List<Wheel> cloneWheels = new ArrayList<>(wheels.size());
+        for (Wheel wheel: wheels) {
+            cloneWheels.add(wheel.clone());
+        }
+        return cloneWheels;
     }
 }
