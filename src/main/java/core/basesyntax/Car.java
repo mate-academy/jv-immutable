398034ed7e511dc4c04a173c3a600d23b0677769
Roman Yuskevych <yuskevych@gmail.com>
2022-11-22 17:14:31 +0200

    added jv-immutable homework solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,17 +1,54 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Make this class immutable. See requirements in task description.
  */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
 
-    //implement this class
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = deepClone(wheels);
+        this.engine = engine == null ? null : engine.clone();
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        return deepClone(wheels);
+    }
+
+    public Engine getEngine() {
+        return engine == null ? null : engine.clone();
+    }
+
+    public Car changeEngine(Engine engine) {
+        return new Car(year, color, wheels, engine);
+    }
+
+    public Car changeColor(String newColor) {
+        return new Car(year, newColor, wheels, engine);
+    }
+
+    public Car addWheel(Wheel newWheel) {
+        List<Wheel> newWheels = getWheels();
+        newWheels.add(newWheel);
+        return new Car(year, color, newWheels, engine);
+    }
 
     @Override
     public String toString() {
@@ -21,5 +58,43 @@ public class Car {
             + ", wheels=" + wheels
             + ", engine=" + engine
             + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (obj.getClass().equals(Car.class)) {
+            Car other = (Car) obj;
+            return this.year == other.year
+                    && Objects.equals(this.color, other.color)
+                    && Objects.equals(this.wheels, other.wheels)
+                    && Objects.equals(this.engine, other.engine);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 101;
+        result = 67 * result + year;
+        result = 67 * result + (color == null ? 0 : color.hashCode());
+        result = 67 * result + (wheels == null ? 0 : wheels.hashCode());
+        result = 67 * result + (engine == null ? 0 : engine.hashCode());
+        return result;
+    }
+
+    private List<Wheel> deepClone(List<Wheel> wheels) {
+        List<Wheel> clonedWheels = new ArrayList<>();
+        if (wheels.size() > 0) {
+            for (Wheel wheel : wheels) {
+                clonedWheels.add(wheel.clone());
+            }
+        }
+        return clonedWheels;
     }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,10 +1,31 @@
 package core.basesyntax;
 
-public class Engine {
+import java.util.Objects;
+
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
-    //implement this class
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
 
     @Override
     public String toString() {
@@ -12,5 +33,38 @@ public class Engine {
             + "horsePower=" + horsePower
             + ", manufacturer='" + manufacturer + '\''
             + '}';
+    }
+
+    @Override
+    public Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Houston, we have a problem", e);
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (obj.getClass().equals(Engine.class)) {
+            Engine other = (Engine) obj;
+            return this.horsePower == other.horsePower
+                    && Objects.equals(this.manufacturer, other.manufacturer);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 13;
+        result = 47 * result + horsePower;
+        result = 47 * result + (manufacturer == null ? 0 : manufacturer.hashCode());
+        return result;
     }
 }
