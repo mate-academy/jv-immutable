455053190cc1316513410e4d560c6c45636d9d29
Nikita Akosnov <nikitaua4226@gmail.com>
2022-12-12 20:59:56 +0200

    Implemented Immutable class

--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,9 +1,11 @@
 package core.basesyntax;
 
-public class Wheel {
+public class Wheel implements Cloneable {
     private int radius;
 
-    //implement this class
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
 
     @Override
     public String toString() {
@@ -11,4 +13,48 @@ public class Wheel {
             + "radius=" + radius
             + '}';
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + radius;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        } 
+        if (obj == null) {
+            return false;
+        }            
+        if (getClass() != obj.getClass()) {
+            return false;
+        }           
+        Wheel other = (Wheel) obj;
+        if (radius != other.radius) {
+            return false;
+        }           
+        return true;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public Wheel clone() {
+        try {
+            return (Wheel) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Cannot clone this Wheel", e);
+        }
+    }   
+    
 }
