3207a8abae5043dc9eb4e786cdb18dea2eb3eb02
Zakharchenko <zakharchenko.andrii@gmail.com>
2023-01-18 10:25:35 +0200

    jv-immutable-hw15-v.1.0

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,17 +1,83 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Make this class immutable. See requirements in task description.
  */
-public class Car {
+public final class Car implements Cloneable {
     private int year;
     private String color;
-    private List<Wheel> wheels;
     private Engine engine;
+    private List<Wheel> wheels;
 
-    //implement this class
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        return wheelsDeepClone(wheelsDeepClone(wheels));///
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = wheels;
+        this.engine = engine.clone();
+    }
+//implement this class
+    public Car changeColor(String newColor) {
+       return new Car(year, newColor, wheelsDeepClone(wheels), engine.clone()); ///
+    }
+
+    public Car addWheel(Wheel newWheel) {
+        if (newWheel != null) {
+            wheels.add(newWheel);
+        }
+        return new Car(year, color, wheelsDeepClone(wheels), engine.clone()); ///
+    }
+
+    public Car changeEngine(Engine engine){
+        if (engine == null) {
+            engine = this.engine;
+        }
+        return new Car(year, color, wheelsDeepClone(wheels), engine);///
+    }
+
+    public  List<Wheel> wheelsDeepClone(List<Wheel> wheels) {
+        List<Wheel> newWheels = new ArrayList<>();
+        for (Wheel wheel : wheels) {
+            newWheels.add(wheel.clone());
+        }
+        return newWheels;
+    }
+
+//qwrijrfvdjlds;s'djjj
+//kxkxcjvhxxlx;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return getYear() == car.getYear() && Objects.equals(getColor(), car.getColor()) && Objects.equals(getWheels(), car.getWheels()) && Objects.equals(getEngine(), car.getEngine());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getYear(), getColor(), getWheels(), getEngine());
+    }
 
     @Override
     public String toString() {
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,10 +1,42 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable{
+
     private int horsePower;
     private String manufacturer;
 
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
     //implement this class
+
+    @Override
+    protected Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Override
     public String toString() {
