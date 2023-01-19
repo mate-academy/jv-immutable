2ff692ae9e47c3ffbbf0ac8c550ff184d2836d07
Oleksandr Kravchenko <oleksandr.kravchenko@spirent.com>
2023-01-20 00:03:24 +0200

    feat: jv-immutable #comment hw

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,10 +1,17 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
-    //implement this class
+    public Engine() {
+
+    }
+
+    public Engine(int horsePower, String manufacturer) {
+        this.manufacturer = manufacturer;
+        this.horsePower = horsePower;
+    }
 
     @Override
     public String toString() {
@@ -12,5 +19,60 @@ public class Engine {
             + "horsePower=" + horsePower
             + ", manufacturer='" + manufacturer + '\''
             + '}';
+    }
+
+    @Override
+    public Engine clone() {
+        try {
+            Engine clone = (Engine) super.clone();
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError(e);
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return this.horsePower + this.manufacturer.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (obj == this) {
+            return true;
+        }
+
+        if (obj.getClass().equals(Engine.class)) {
+            Engine casted = (Engine) obj;
+            if (casted.horsePower == this.horsePower && (
+                    this.manufacturer == null && casted.manufacturer == null
+                    || (this.manufacturer != null && this.manufacturer.equals(casted.manufacturer))
+                )
+            ) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
     }
 }
