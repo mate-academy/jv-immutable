501ab29f3154004220134234c8ec74e3953154b2
Stepan Peknyi <stepan_peknyy@ukr.net>
2023-01-23 22:08:08 +0200

    solution of task Immutable

--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,9 +1,45 @@
 package core.basesyntax;
 
-public class Wheel {
+public class Wheel implements Cloneable {
     private int radius;
 
-    //implement this class
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public int hashCode() {
+        return radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Wheel wheel = (Wheel) obj;
+        return radius == wheel.radius;
+    }
+
+    @Override
+    protected Wheel clone() {
+        try {
+            return (Wheel) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Cant clone Wheel" + e);
+        }
+    }
 
     @Override
     public String toString() {
