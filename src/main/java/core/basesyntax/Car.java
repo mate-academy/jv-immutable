cebf46822d01aa8fe2afe71439fa90326e7fbbcf
Artem Bordichuk <artyombordichuk@gmail.com>
2023-02-10 00:45:27 +0200

    Commit 1: 1. The method `changeEngine()`,`changeColor()`, `addWheel()` were implemented. 2. The `equals()` and `hashCode()` methods and constructor with all arguments in all classes were implemented, in order for the tests to run correctly.

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,17 +1,47 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Make this class immutable. See requirements in task description.
  */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
 
     //implement this class
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        if (wheels == null) {
+            throw new NullPointerException("Can't create a car without wheels " + wheels);
+        }
+        this.wheels = new Wheel().getClone(wheels);
+        this.engine = engine != null ? engine.clone() : null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return year == car.year && Objects.equals(color, car.color)
+                && Objects.equals(wheels, car.wheels)
+                && Objects.equals(engine, car.engine);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(year, color, wheels, engine);
+    }
 
     @Override
     public String toString() {
@@ -21,5 +51,43 @@ public class Car {
             + ", wheels=" + wheels
             + ", engine=" + engine
             + '}';
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        List<Wheel> cloneWheelsList = new ArrayList<>(wheels.size());
+
+        for (Wheel wheel : wheels) {
+            cloneWheelsList.add(wheel.clone());
+        }
+        return cloneWheelsList;
+    }
+
+    public Engine getEngine() {
+        if (this.engine == null) {
+            return null;
+        }
+        return engine.clone();
+    }
+
+    public Car changeEngine(Engine engine) {
+        return new Car(this.year, this.color, getWheels(), engine);
+    }
+
+    public Car changeColor(String newColor) {
+        return new Car(this.year, newColor, getWheels(), getEngine());
+    }
+
+    public Car addWheel(Wheel newWheel) {
+        Car newCar = new Car(this.year, this.color, getWheels(), getEngine());
+        newCar.wheels.add(newWheel);
+        return newCar;
     }
 }
