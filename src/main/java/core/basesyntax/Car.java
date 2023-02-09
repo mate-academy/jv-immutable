8a26f67a6f03f1a4570f014658db2035b8fbf84c
Алексей <m.leshka955@gmail.com>
2023-02-09 16:47:31 +0200

    Implement method: changeEngine(Engine engine) addWheel(Wheel newWheel) changeColor(Strung newColor) equals hashCode

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,25 +1,95 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-/**
- * Make this class immutable. See requirements in task description.
- */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
 
-    //implement this class
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = getCopy(wheels);
+        this.engine = cloneEngine(engine);
+    }
+
+    public Car changeEngine(Engine engine) {
+        return new Car(year, color, wheels, engine);
+    }
+
+    public Car addWheel(Wheel newWheel) {
+        List<Wheel> newWheels = getCopy(wheels);
+        newWheels.add(newWheel);
+        return new Car(year, color, newWheels, engine);
+    }
+
+    public Car changeColor(String newColor) {
+        return new Car(year, newColor, wheels, engine);
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        return getCopy(wheels);
+    }
+
+    public Engine getEngine() {
+        return cloneEngine(engine);
+    }
 
     @Override
     public String toString() {
         return "Car{"
-            + "year=" + year
-            + ", color='" + color + '\''
-            + ", wheels=" + wheels
-            + ", engine=" + engine
-            + '}';
+                + "year=" + year
+                + ", color='" + color + '\''
+                + ", wheels=" + wheels
+                + ", engine=" + engine
+                + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int result = year;
+        result = 17 * result + (color != null ? color.hashCode() : 0);
+        result = 17 * result + (wheels != null ? wheels.hashCode() : 0);
+        result = 17 * result + (engine != null ? engine.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Car car = (Car) obj;
+        return year == car.year &&
+                Objects.equals(color, car.color) &&
+                Objects.equals(wheels, car.wheels) &&
+                Objects.equals(engine, car.engine);
+    }
+
+    private Engine cloneEngine(Engine engine) {
+        return engine != null ? engine.clone() : null;
+    }
+
+    private List<Wheel> getCopy(List<Wheel> wheels) {
+        List<Wheel> wheel = new ArrayList<>(wheels.size());
+        for (Wheel wheel1 : wheels) {
+            wheel.add(wheel1.clone());
+        }
+        return wheel;
     }
 }
