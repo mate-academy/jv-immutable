9f23f172272f061f38b7529c6339b86e765b8363
Yaroslav Chornyi <123074518+jaroslavcyk@users.noreply.github.com>
2023-02-25 19:05:33 +0200

    Update Engine.java

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -37,7 +37,7 @@ public class Engine implements Cloneable {
 
     @Override
     public boolean equals(Object o) {
-         if (this == o) {
+        if (this == o) {
             return true;
         }
         if (o == null || getClass() != o.getClass()) {
