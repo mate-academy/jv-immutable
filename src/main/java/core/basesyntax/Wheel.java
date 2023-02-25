d6984d21407171281af52bb940f918106efb4579
Yaroslav Chornyi <123074518+jaroslavcyk@users.noreply.github.com>
2023-02-25 19:05:44 +0200

    Update Wheel.java

--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -19,7 +19,7 @@ public class Wheel implements Cloneable {
 
     @Override
     public boolean equals(Object o) {
-         if (this == o) {
+        if (this == o) {
             return true;
         }
         if (o == null || getClass() != o.getClass()) {
