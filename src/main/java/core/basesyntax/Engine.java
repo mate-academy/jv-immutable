b4fd02264d566483840b432266700acf7540166a
Yaroslav Kopiyka <19yar96.creedgmail.com>
2023-02-28 16:07:09 +0200

    immutable

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,10 +1,64 @@
 package core.basesyntax;
 
-public class Engine {
+import java.util.Objects;
+
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
-    //implement this class
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == this) {
+            return true;
+        }
+        if (object == null) {
+            return false;
+        }
+        if (object.getClass().equals(Engine.class)) {
+            Engine currentEngine = (Engine) object;
+            return (horsePower == currentEngine.horsePower)
+                    && Objects.equals(manufacturer, currentEngine.manufacturer);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + horsePower;
+        result = 31 * result + (manufacturer == null ? 0 : manufacturer.hashCode());
+        return result;
+    }
+
+    @Override
+    public Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't create clone of engine ", e);
+        }
+    }
 
     @Override
     public String toString() {
@@ -13,4 +67,6 @@ public class Engine {
             + ", manufacturer='" + manufacturer + '\''
             + '}';
     }
+
+
 }
