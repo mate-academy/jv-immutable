e234c7f0f7a267747fef4ed7d8ec573d3f8996df
Volodymyr Sakalosh <sva2005@ukr.net>
2023-03-11 23:01:10 +0200

    Override methods clone(), equals(), hashCode(). Created constructor, methods getters and setters.

--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,14 +1,54 @@
 package core.basesyntax;
 
-public class Wheel {
+public class Wheel implements Cloneable {
     private int radius;
 
-    //implement this class
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    protected Wheel clone()  {
+        try {
+            return (Wheel) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Don't clone from Wheel object", e);
+        }
+    }
 
     @Override
     public String toString() {
         return "Wheel{"
             + "radius=" + radius
             + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        if (radius != 0) {
+            result = 31 * result + radius;
+        }
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Wheel other = (Wheel) obj;
+        return radius == other.radius;
     }
 }
