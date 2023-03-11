aff09e9990314bba5c3b74c78a501516e8e7637b
Volodymyr Sakalosh <sva2005@ukr.net>
2023-03-11 23:45:15 +0200

    Fixed constructor

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -14,7 +14,7 @@ public final class Car {
         this.year = year;
         this.color = color;
         this.wheels = new ArrayList<>(wheels);
-        this.engine = engine.clone();
+        this.engine = engine;
     }
 
     public Car addWheel(Wheel newWheel) {
