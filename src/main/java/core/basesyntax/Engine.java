320cafb7cfb7a53cf06c83caff23ca56e907358b
Volodymyr Sakalosh <sva2005@ukr.net>
2023-03-11 23:01:02 +0200

    Override methods clone(), equals(), hashCode(). Created constructor, methods getters and setters.

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,10 +1,40 @@
 package core.basesyntax;
 
-public class Engine {
+import java.util.Objects;
+
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
-    //implement this class
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    protected Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Don't clone from Engine object", e);
+        }
+    }
 
     @Override
     public String toString() {
@@ -12,5 +42,31 @@ public class Engine {
             + "horsePower=" + horsePower
             + ", manufacturer='" + manufacturer + '\''
             + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        if (manufacturer != null) {
+            result = result * 31 + manufacturer.hashCode();
+        }
+        if (horsePower != 0) {
+            result = result * 31 + horsePower;
+        }
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Engine other = (Engine) obj;
+        boolean isHorsePower = Objects.equals(horsePower, other.horsePower);
+        boolean isManufacturer = Objects.equals(manufacturer, other.manufacturer);
+        return isHorsePower && isManufacturer;
     }
 }
