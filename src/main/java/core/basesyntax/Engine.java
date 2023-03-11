64f2d61157d0799fbf51028cf0bf0aea93db3db0
Volodymyr Sakalosh <sva2005@ukr.net>
2023-03-11 23:52:16 +0200

    Deleted free constructor

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -6,11 +6,6 @@ public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
-    public Engine() {
-        horsePower = 0;
-        manufacturer = null;
-    }
-
     public Engine(int horsePower, String manufacturer) {
         this.horsePower = horsePower;
         this.manufacturer = manufacturer;
