094ec4541c5fc6a4deca8af5f56d71f1825f2a84
Volodymyr Sakalosh <sva2005@ukr.net>
2023-03-11 22:59:23 +0200

    Created methods addWheel(), changeColor(), changeEngine(), constructor. Implemented methods getYear(), getColor(), getWheels(), getEngine(). Changed fields private and final

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,17 +1,54 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Make this class immutable. See requirements in task description.
- */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
 
-    //implement this class
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = new ArrayList<>(wheels);
+        this.engine = engine.clone();
+    }
+
+    public Car addWheel(Wheel newWheel) {
+        List<Wheel> list = new ArrayList<>();
+        list.add(newWheel);
+        return new Car(getYear(), getColor(), list, getEngine());
+    }
+
+    public Car changeColor(String newColor) {
+        return new Car(getYear(), newColor, getWheels(), getEngine());
+    }
+
+    public Car changeEngine(Engine engine) {
+        return new Car(getYear(), getColor(), getWheels(), engine);
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        List<Wheel> list = new ArrayList<>(wheels.size());
+        for (Wheel wheel : wheels) {
+            list.add(wheel.clone());
+        }
+        return list;
+    }
+
+    public Engine getEngine() {
+        return engine.clone();
+    }
 
     @Override
     public String toString() {
