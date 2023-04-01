ca728bf6eefd1ce1cf720c97a0becc69705ba55a
Sergey Zavalniuk <sergzavalnyuk@gmail.com>
2023-04-01 20:12:08 +0300

    Implemented method changeEngine(Engine engine); Implemented method changeColor(String newColor); Implemented method addWheel(Wheel newWheel)

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,10 +1,61 @@
 package core.basesyntax;
 
-public class Engine {
+import java.util.Objects;
+
+public final class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
-    //implement this class
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Engine engine = (Engine) o;
+        return horsePower == engine.horsePower && Objects.equals(manufacturer, engine.manufacturer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(horsePower, manufacturer);
+    }
+
+    @Override
+    protected Engine clone() {
+        Engine cloned;
+        try {
+            cloned = (Engine) super.clone();
+            cloned.setManufacturer(this.manufacturer);
+            cloned.setHorsePower(this.horsePower);
+            return cloned;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can`t create clone Engine", e);
+        }
+    }
 
     @Override
     public String toString() {
