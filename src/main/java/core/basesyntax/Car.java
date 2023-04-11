ff9d06f784e1216c28658c21f9a97e66fcb3c3ae
Oles <a.vykhovanok@gmail.com>
2023-04-11 16:57:32 +0300

    solved task

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,17 +1,101 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Make this class immutable. See requirements in task description.
  */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
 
-    //implement this class
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = new ArrayList<>(wheels.size());
+        for (Wheel wheel : wheels) {
+            this.wheels.add(wheel.clone());
+        }
+        if (engine == null) {
+            this.engine = null;
+        } else {
+            this.engine = engine.clone();
+        }
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        List<Wheel> cloned = new ArrayList<>(wheels.size());
+        for (Wheel wheel : wheels) {
+            cloned.add(wheel.clone());
+        }
+        return cloned;
+    }
+
+    public Engine getEngine() {
+        if (engine == null) {
+            return null;
+        } else {
+            return engine.clone();
+        }
+    }
+
+    public Car changeEngine(Engine engine) {
+        return new Car(getYear(), getColor(), getWheels(), engine);
+    }
+
+    public Car changeColor(String newColor) {
+        return new Car(getYear(), newColor, getWheels(), getEngine());
+    }
+
+    public Car addWheel(Wheel newWheel) {
+        List<Wheel> modifiedWheels = new ArrayList<>(getWheels());
+        modifiedWheels.add(newWheel);
+        return new Car(getYear(), getColor(), modifiedWheels, getEngine());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Car)) {
+            return false;
+        }
+
+        Car car = (Car) o;
+
+        if (year != car.year) {
+            return false;
+        }
+        if (!Objects.equals(color, car.color)) {
+            return false;
+        }
+        if (!Objects.equals(wheels, car.wheels)) {
+            return false;
+        }
+        return Objects.equals(engine, car.engine);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = year;
+        result = 31 * result + (color != null ? color.hashCode() : 0);
+        result = 31 * result + (wheels != null ? wheels.hashCode() : 0);
+        result = 31 * result + (engine != null ? engine.hashCode() : 0);
+        return result;
+    }
 
     @Override
     public String toString() {
