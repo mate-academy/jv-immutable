cf48000d1b086db8330684f7e6db41cdfbdf016d
Taras Machyta <taras.machyta.sa@gmail.com>
2023-05-09 15:20:24 +0300

    Solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,17 +1,39 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Make this class immutable. See requirements in task description.
  */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
+
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = getCopyWheel(wheels);
+        this.engine = getCopyEngine(engine);
+    }
 
     //implement this class
+    public Car changeEngine(Engine engine) {
+        return new Car(year, color, wheels, engine.clone());
+    }
+
+    public Car changeColor(String newColor) {
+        return new Car(year, newColor, wheels, engine.clone());
+    }
+
+    public Car addWheel(Wheel newWheel) {
+        List<Wheel> addednewWheel = new ArrayList<>(wheels);
+        addednewWheel.add(newWheel);
+        return new Car(year, color, addednewWheel, engine.clone());
+    }
 
     @Override
     public String toString() {
@@ -21,5 +43,55 @@ public class Car {
             + ", wheels=" + wheels
             + ", engine=" + engine
             + '}';
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    private static List<Wheel> getCopyWheel(List<Wheel> wheels) {
+        List<Wheel> wheelCopy = new ArrayList<>(wheels.size());
+        for (Wheel wheel : wheels) {
+            wheelCopy.add(wheel.clone());
+        }
+        return wheelCopy;
+    }
+
+    public List<Wheel> getWheels() {
+        return getCopyWheel(wheels);
+    }
+
+    private static Engine getCopyEngine(Engine engine) {
+        if (engine == null) {
+            return null;
+        }
+        return engine.clone();
+    }
+
+    public Engine getEngine() {
+        return getCopyEngine(engine);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return year == car.year && Objects.equals(color, car.color)
+                && Objects.equals(wheels, car.wheels)
+                && Objects.equals(engine, car.engine);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(year, color, wheels, engine);
     }
 }
