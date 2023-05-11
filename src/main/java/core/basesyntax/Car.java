ff75b79a4b153a24567bd5c73773f8042b5e2665
Chris <tynkastya@gmail.com>
2023-05-11 19:10:27 +0300

    implemented task

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,17 +1,66 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Make this class immutable. See requirements in task description.
  */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car implements Cloneable {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
 
-    //implement this class
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = getCloneWheels(wheels);
+        this.engine = (engine != null) ? engine.clone() : null;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        return getCloneWheels(wheels);
+    }
+
+    public List<Wheel> getCloneWheels(List<Wheel> wheels) {
+        List<Wheel> copeWheels = new ArrayList<>();
+        for (Wheel wheel : wheels) {
+            copeWheels.add(wheel.clone());
+        }
+        return copeWheels;
+    }
+
+    public Engine getEngine() {
+        if (engine != null) {
+            return engine.clone();
+        } else {
+            return null;
+        }
+    }
+
+    public Car changeEngine(Engine engine) {
+        return new Car(year, color, wheels, engine);
+    }
+
+    public Car changeColor(String newColor) {
+        return new Car(year, newColor, wheels, engine);
+    }
+
+    public Car addWheel(Wheel newWheel) {
+        List<Wheel> temp = new ArrayList<>(wheels);
+        temp.add(newWheel);
+        return new Car(year, color, temp, engine);
+    }
 
     @Override
     public String toString() {
@@ -21,5 +70,34 @@ public class Car {
             + ", wheels=" + wheels
             + ", engine=" + engine
             + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Car)) {
+            return false;
+        }
+        Car car = (Car) o;
+        return getYear() == car.getYear()
+                && Objects.equals(getColor(), car.getColor())
+                && Objects.equals(getWheels(), car.getWheels())
+                && Objects.equals(getEngine(), car.getEngine());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getYear(), getColor(), getWheels(), getEngine());
+    }
+
+    @Override
+    protected Car clone() {
+        try {
+            return (Car) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't create clone of Car object", e);
+        }
     }
 }
