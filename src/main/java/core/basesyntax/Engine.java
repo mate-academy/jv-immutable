bd237dce9112909f7089cd6ec841587fd31aade1
Viktoriia Semenchenko <vickysemenchenko@gmail.com>
2023-05-16 12:19:10 +0300

    done task

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,16 +1,71 @@
 package core.basesyntax;
 
-public class Engine {
+import java.util.Objects;
+
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
-    //implement this class
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    public boolean equals(Object engine) {
+        if (engine == this) {
+            return true;
+        }
+        if (engine == null) {
+            return false;
+        }
+        if (engine.getClass().equals(Engine.class)) {
+            Engine current = (Engine) engine;
+            return this.horsePower == current.horsePower
+                    && (Objects.equals(this.manufacturer, current.manufacturer));
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + horsePower;
+        result = 31 * result
+                + (manufacturer == null ? 0 : manufacturer.hashCode());
+        return result;
+    }
 
     @Override
     public String toString() {
         return "Engine{"
-            + "horsePower=" + horsePower
-            + ", manufacturer='" + manufacturer + '\''
-            + '}';
+                + "horsePower=" + horsePower
+                + ", manufacturer='" + manufacturer + '\''
+                + '}';
+    }
+
+    @Override
+    public Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can not create Engine clone ", e);
+        }
     }
 }
