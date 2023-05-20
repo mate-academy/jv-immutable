c0c0371fa3d107c430b05152e07bb27d5536a1b5
Artem <cr7realartem@gmail.com>
2023-05-20 12:25:19 +0300

    done

--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,9 +1,50 @@
 package core.basesyntax;
 
-public class Wheel {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Wheel implements Cloneable{
     private int radius;
 
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+
+
     //implement this class
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Wheel wheel = (Wheel) o;
+        return radius == wheel.radius;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(radius);
+    }
+
+    @Override
+    public Wheel clone() {
+        try {
+            return (Wheel) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Override
     public String toString() {
@@ -11,4 +52,5 @@ public class Wheel {
             + "radius=" + radius
             + '}';
     }
+
 }
