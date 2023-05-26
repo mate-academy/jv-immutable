cfe925175019bc842495f47ebca3851f6f2e3c10
pochtalon <pochtalon1010@gmail.com>
2023-05-26 20:49:31 +0300

    ytfkjyg

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,17 +1,50 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Make this class immutable. See requirements in task description.
  */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
 
-    //implement this class
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        if (wheels == null) {
+            throw new NullPointerException();
+        }
+        this.year = year;
+        this.color = color;
+        this.engine = engine;
+        this.wheels = wheels;
+    }
+
+    public int getYear() {
+        return Integer.valueOf(year);
+    }
+
+    public String getColor() {
+        return String.valueOf(color);
+    }
+
+    public List<Wheel> getWheels() {
+        List<Wheel> forReturn = new ArrayList<>(wheels.size());
+        for (Wheel wheel : wheels) {
+            forReturn.add(new Wheel(wheel.getRadius()));
+        }
+        return forReturn;
+    }
+
+    public Engine getEngine() {
+        if (engine == null) {
+            return null;
+        }
+        return engine.clone();
+    }
 
     @Override
     public String toString() {
@@ -21,5 +54,32 @@ public class Car {
             + ", wheels=" + wheels
             + ", engine=" + engine
             + '}';
+    }
+
+    public Car addWheel(Wheel wheel) {
+        List<Wheel> whee = getWheels();
+        whee.add(wheel);
+        return new Car(year, color, whee, engine);
+    }
+
+    public Car changeEngine(Engine eng) {
+        return new Car(year, color, wheels, eng);
+    }
+
+    public Car changeColor(String expected) {
+        return new Car(year, expected, wheels, engine);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return getYear() == car.getYear() && Objects.equals(getColor(), car.getColor()) && Objects.equals(getWheels(), car.getWheels()) && Objects.equals(getEngine(), car.getEngine());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getYear(), getColor(), getWheels(), getEngine());
     }
 }
--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,14 +1,43 @@
 package core.basesyntax;
 
-public class Wheel {
+import java.util.Objects;
+
+public class Wheel implements Cloneable {
     private int radius;
 
-    //implement this class
+    public Wheel(int radius) {
+        this.radius = Integer.valueOf(radius);
+    }
+
+    public int getRadius() {
+        return Integer.valueOf(radius);
+    }
 
     @Override
     public String toString() {
         return "Wheel{"
             + "radius=" + radius
             + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Wheel wheel = (Wheel) o;
+        return getRadius() == wheel.getRadius();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getRadius());
+    }
+
+    public void setRadius(int i) {
+    }
+
+    @Override
+    protected Wheel clone()  {
+        return new Wheel(radius);
     }
 }
