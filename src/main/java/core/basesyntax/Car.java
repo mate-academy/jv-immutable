f27fd59c44ff65356c9785e8c570fd8373934950
Artem <artemenko.as@gmail.com>
2023-05-31 20:54:17 +0300

    Added logic to classes: Car, Engine and Wheel. Class Car is immutable.

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,25 +1,101 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Make this class immutable. See requirements in task description.
  */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car implements Cloneable {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
+
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = new ArrayList<>(wheels.size());
+        for (Wheel wheel : wheels) {
+            this.wheels.add(wheel.clone());
+        }
+        if (engine == null) {
+            this.engine = null;
+        } else {
+            this.engine = engine.clone();
+        }
+    }
+
+    public Car changeEngine(Engine engine) {
+        return new Car(this.year, this.color, this.wheels, engine);
+    }
+
+    public Car changeColor(String newColor) {
+        return new Car(this.year, newColor, this.wheels, this.engine);
+    }
+
+    public Car addWheel(Wheel newWheel) {
+        List<Wheel> newWheels = new ArrayList<>(this.wheels);
+        newWheels.add(newWheel);
+        return new Car(this.year, this.color, newWheels, this.engine);
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        List<Wheel> newWheels = new ArrayList<>(wheels.size());
+        for (Wheel wheel : wheels) {
+            newWheels.add(wheel.clone());
+        }
+        return newWheels;
+    }
+
+    public Engine getEngine() {
+        return engine == null ? null : engine.clone();
+    }
+
+    @Override
+    protected Car clone() {
+        List<Wheel> clonedWheels = getWheels();
+        return new Car(this.year, this.color, clonedWheels, getEngine());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Car)) {
+            return false;
+        }
+        Car car = (Car) o;
+        return getYear() == car.getYear()
+                && Objects.equals(getColor(), car.getColor())
+                && Objects.equals(getWheels(), car.getWheels())
+                && Objects.equals(getEngine(), car.getEngine());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getYear(), getColor(), getWheels(), getEngine());
+    }
 
     //implement this class
 
     @Override
     public String toString() {
         return "Car{"
-            + "year=" + year
-            + ", color='" + color + '\''
-            + ", wheels=" + wheels
-            + ", engine=" + engine
-            + '}';
+                + "year=" + year
+                + ", color='" + color + '\''
+                + ", wheels=" + wheels
+                + ", engine=" + engine
+                + '}';
     }
 }
