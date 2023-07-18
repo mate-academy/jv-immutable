d28f6422fdec570b51b21357319cc819e8772f0a
Oleg Turyk <oleh.turyk.ki.2019@lpnu.ua>
2023-07-18 14:19:18 +0300

    made Car immutable

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,10 +1,62 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
-    //implement this class
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    public Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't clone", e);
+        }
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Engine)) {
+            return false;
+        }
+
+        Engine engine = (Engine) o;
+
+        if (horsePower != engine.horsePower) {
+            return false;
+        }
+        return manufacturer.equals(engine.manufacturer);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = horsePower;
+        result = 31 * result + manufacturer.hashCode();
+        return result;
+    }
 
     @Override
     public String toString() {
