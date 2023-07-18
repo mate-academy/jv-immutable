876031978d9b3380e82a19bb1bafb4a16e7da75a
Oleg Stashkiv <stashkivolegg@gmail.com>
2023-07-18 15:26:05 +0300

    Making a class Car immutable. Implemented methods changeEngine, changeColor, addWheel. Implemented methods equals() and hashCode() for all classes.

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,17 +1,100 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Make this class immutable. See requirements in task description.
  */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
 
-    //implement this class
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = copyWheels(wheels);
+        if (engine == null) {
+            this.engine = null;
+        } else {
+            this.engine = engine.clone();
+        }
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        return copyWheels(wheels);
+    }
+
+    public Engine getEngine() {
+        if (engine == null) {
+            return null;
+        }
+        return engine.clone();
+    }
+
+    public Car changeEngine(Engine engine) {
+        return new Car(year, color, wheels, engine);
+    }
+
+    public Car changeColor(String newColor) {
+        return new Car(year, newColor, wheels, engine);
+    }
+
+    public Car addWheel(Wheel newWheel) {
+        List<Wheel> wheelsNew = copyWheels(wheels);
+        wheelsNew.add(newWheel);
+        return new Car(year, color, wheelsNew, engine);
+    }
+
+    private List<Wheel> copyWheels(List<Wheel> wheels) {
+        List<Wheel> wheelsCopy = new ArrayList<>(wheels.size());
+        for (Wheel wheel : wheels) {
+            wheelsCopy.add(wheel.clone());
+        }
+        return wheelsCopy;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Car car = (Car) o;
+
+        if (year != car.year) {
+            return false;
+        }
+        if (!color.equals(car.color)) {
+            return false;
+        }
+        if (!wheels.equals(car.wheels)) {
+            return false;
+        }
+        return engine.equals(car.engine);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = year;
+        result = 31 * result + color.hashCode();
+        result = 31 * result + wheels.hashCode();
+        result = 31 * result + engine.hashCode();
+        return result;
+    }
 
     @Override
     public String toString() {
