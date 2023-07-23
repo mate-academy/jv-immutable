3e3fc99d5848e221461716cec30bc674d4de83b7
Olena Protsailo <olenaprotsailo@gmail.com>
2023-07-23 09:31:02 +0300

    maked Car immutable

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,17 +1,54 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Make this class immutable. See requirements in task description.
  */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
+
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = getCopy(wheels);
+        this.engine = engine == null ? null : engine.clone();
+    }
+
 
     //implement this class
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + year;
+        result = 31 * result + (color == null ? 0 : color.hashCode());
+        result = 31 * result + (wheels == null ? 0 : wheels.hashCode());
+        result = 31 * result + (engine == null ? 0 : engine.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object car) {
+        if (car == this) {
+            return true;
+        }
+        if (car == null) {
+            return false;
+        }
+        if (car instanceof Car) {
+            Car current = (Car) car;
+            return Objects.equals(color,current.color) && Objects.equals(wheels,current.wheels)
+                    && Objects.equals(engine,current.engine) && year == current.year;
+        }
+        return false;
+
+    }
 
     @Override
     public String toString() {
@@ -22,4 +59,46 @@ public class Car {
             + ", engine=" + engine
             + '}';
     }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        return getCopy(wheels);
+    }
+
+    public Engine getEngine() {
+        // deep copy engine
+        return engine == null ? null : engine.clone();
+    }
+
+    public Car changeEngine(Engine engine) {
+        return new Car(year,color,wheels,engine);
+    }
+
+    public Car changeColor(String newColor) {
+        return new Car(year,newColor,wheels,engine);
+    }
+
+    public Car addWheel(Wheel newWheel) {
+        Wheel wheel = new Wheel(newWheel.getRadius());
+        List<Wheel> wheelsNew = new ArrayList<>(wheels.size());
+        wheelsNew.addAll(wheels);
+        wheelsNew.add(wheel);
+        return new Car(year,color,wheelsNew,engine);
+    }
+
+    private static List<Wheel> getCopy(List<Wheel> wheels) {
+        List<Wheel> wheelsCopy = new ArrayList<>(wheels.size());
+        for (Wheel wheel : wheels) {
+            wheelsCopy.add(wheel.clone());
+        }
+        return wheelsCopy;
+    }
 }
+
