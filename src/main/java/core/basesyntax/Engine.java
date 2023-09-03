42505b48d779956699b1b88b919ff5254b23fb6f
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-09-04 01:12:38 +0300

    added changes to the class Engine

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -38,8 +38,12 @@ public class Engine implements Cloneable {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Engine engine = (Engine) o;
         return horsePower == engine.horsePower && Objects.equals(manufacturer, engine.manufacturer);
     }
@@ -51,9 +55,15 @@ public class Engine implements Cloneable {
 
     @Override
     public String toString() {
-        return "Engine{" +
-                "horsePower=" + horsePower +
-                ", manufacturer='" + manufacturer + '\'' +
+        return "Engine{"
+                +
+                "horsePower="
+                + horsePower
+                +
+                ", manufacturer='"
+                + manufacturer
+                + '\''
+                +
                 '}';
     }
 }
