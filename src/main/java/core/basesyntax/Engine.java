70ec11f3bc0b5712dd302e593aa7e7de62954e80
Petro Smoliar <smp22pl@gmail.com>
2023-09-08 12:40:13 +0200

    Implemented ChangeEngine method Implemented changeColor method Implemented addWheel method

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,10 +1,51 @@
 package core.basesyntax;
 
-public class Engine {
+import java.util.Objects;
+
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
-    //implement this class
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Engine)) {
+            return false;
+        }
+        Engine engine = (Engine) o;
+        return horsePower == engine.horsePower
+                && Objects.equals(manufacturer, engine.manufacturer);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * horsePower + manufacturer.hashCode();
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public Engine setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+        return this;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public Engine setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+        return this;
+    }
 
     @Override
     public String toString() {
@@ -12,5 +53,10 @@ public class Engine {
             + "horsePower=" + horsePower
             + ", manufacturer='" + manufacturer + '\''
             + '}';
+    }
+
+    @Override
+    public Engine clone() {
+        return new Engine(horsePower, manufacturer);
     }
 }
