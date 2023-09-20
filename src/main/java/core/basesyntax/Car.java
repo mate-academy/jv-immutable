b0484c2c86a434e8cc3d2a805e8726091da06617
IhorTrokhymchuk <hameleon009@gmail.com>
2023-09-21 00:28:20 +0300

    fixed code

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -4,14 +4,50 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * Make this class immutable. See requirements in task description.
+ */
 public final class Car {
     private final int year;
     private final String color;
     private final List<Wheel> wheels;
     private final Engine engine;
 
-    public Car changeEngine(Engine newEngine) {
-        return new Car(year, color, wheels, newEngine);
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = new ArrayList<>();
+        for (Wheel wheel : wheels) {
+            this.wheels.add(wheel.clone());
+        }
+        this.engine = engine == null ? engine : engine.clone();
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        List<Wheel> clonedWheels = new ArrayList<>();
+        for (Wheel wheel : wheels) {
+            clonedWheels.add(wheel.clone());
+        }
+        return clonedWheels;
+    }
+
+    public Engine getEngine() {
+        if (engine != null) {
+            return engine.clone();
+        }
+        return null;
+    }
+
+    public Car changeEngine(Engine engine) {
+        return new Car(year, color, wheels, engine);
     }
 
     public Car changeColor(String newColor) {
@@ -24,29 +60,6 @@ public final class Car {
         return new Car(year, color, newWheels, engine);
     }
 
-    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
-        this.year = year;
-        this.color = color;
-        this.wheels = new ArrayList<>(wheels);
-        this.engine = engine;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public List<Wheel> getWheels() {
-        return List.copyOf(wheels);
-    }
-
-    public Engine getEngine() {
-        return engine;
-    }
-
     @Override
     public int hashCode() {
         return Objects.hash(year, color, wheels, engine);
@@ -57,13 +70,15 @@ public final class Car {
         if (this == object) {
             return true;
         }
-        if (object == null || getClass() != object.getClass()) {
+        if (!(object instanceof Car)) {
             return false;
         }
-        Car otherCar = (Car) object;
-        return year == otherCar.year && Objects.equals(color, otherCar.color)
-                && Objects.equals(wheels, otherCar.wheels)
-                && Objects.equals(engine, otherCar.engine);
+        Car cars = (Car) object;
+
+        return Objects.equals(cars.year, year)
+                && Objects.equals(cars.color, color)
+                && cars.wheels.equals(wheels)
+                && Objects.equals(cars.engine, engine);
     }
 
     @Override
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,6 +1,8 @@
 package core.basesyntax;
 
-public class Engine {
+import java.util.Objects;
+
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
@@ -13,19 +15,45 @@ public class Engine {
         return horsePower;
     }
 
-    public void setHorsePower(int horsePower) {
-        this.horsePower = horsePower;
-    }
-
     public String getManufacturer() {
         return manufacturer;
+    }
+
+    @Override
+    protected Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError("Clone not supported");
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(horsePower, manufacturer);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (!(object instanceof Engine)) {
+            return false;
+        }
+        Engine eng = (Engine) object;
+
+        return Objects.equals(horsePower, eng.horsePower)
+                && Objects.equals(manufacturer, eng.manufacturer);
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
     }
 
     public void setManufacturer(String manufacturer) {
         this.manufacturer = manufacturer;
     }
-
-    //implement this class
 
     @Override
     public String toString() {
