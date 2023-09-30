9ecbdd0024363cf479744e2074d2971f24bf7aed
Artem Peiev <slaybrute@gmail.com>
2023-10-01 01:11:03 +0300

    Added solution

--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,14 +1,45 @@
 package core.basesyntax;
 
-public class Wheel {
+public class Wheel implements Cloneable {
     private int radius;
 
-    //implement this class
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return this.radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
 
     @Override
     public String toString() {
         return "Wheel{"
-            + "radius=" + radius
-            + '}';
+                + "radius=" + radius
+                + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+        Wheel newWheel = (Wheel) o;
+        return this.radius == newWheel.radius;
+    }
+
+    @Override
+    public int hashCode() {
+        return radius;
+    }
+
+    public Wheel clone() {
+        return new Wheel(radius);
     }
 }
