aafc6855fa63f909baa781a4cb32505fbc2c554f
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2023-10-09 18:11:00 +0300

    implemented the code

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,10 +1,16 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
     //implement this class
+
+
+    public Engine(int horsePower, String manufacturer) {
+        this.manufacturer = manufacturer;
+        this.horsePower = horsePower;
+    }
 
     @Override
     public String toString() {
@@ -12,5 +18,30 @@ public class Engine {
             + "horsePower=" + horsePower
             + ", manufacturer='" + manufacturer + '\''
             + '}';
+    }
+
+    @Override
+    protected Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Engine is not cloneable");
+        }
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
     }
 }
