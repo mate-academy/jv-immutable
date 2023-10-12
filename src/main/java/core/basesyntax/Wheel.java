e144a9eb9fedebd2e376b87ad88ede7236e26186
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2023-10-12 21:55:35 +0300

    Implemented 3 methods to car class and made it immutable

--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,14 +1,56 @@
 package core.basesyntax;
 
-public class Wheel {
+import java.util.Objects;
+
+public class Wheel implements Cloneable {
     private int radius;
 
     //implement this class
+
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    @Override
+    public Wheel clone() {
+        return new Wheel(radius);
+    }
 
     @Override
     public String toString() {
         return "Wheel{"
             + "radius=" + radius
             + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(radius);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null) {
+            return false;
+        }
+
+        if (obj.getClass().equals(getClass())) {
+            Wheel current = (Wheel) obj;
+            return Objects.equals(current.radius, this.radius);
+        }
+
+        return false;
     }
 }
