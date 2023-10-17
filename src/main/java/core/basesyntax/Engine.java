1902fd4af00b769993df854948e57d0524685375
Vladyslav Herasymenko <dalv23092005@gmail.com>
2023-10-17 23:17:55 +0200

    SUPER

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,10 +1,57 @@
 package core.basesyntax;
 
-public class Engine {
+import java.util.Objects;
+
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
     //implement this class
+
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    public int hashCode() {
+        return (Objects.hashCode(horsePower) + Objects.hashCode(manufacturer)) * 7;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+
+        if (obj == null) {
+            return false;
+        }
+
+        if (obj.getClass().equals(Engine.class)) {
+            Engine engine = (Engine) obj;
+            return (engine.getHorsePower() == this.getHorsePower()
+                    && engine.getManufacturer() == this.getManufacturer())
+                    || obj.hashCode() == this.hashCode();
+        }
+        return false;
+    }
 
     @Override
     public String toString() {
@@ -12,5 +59,10 @@ public class Engine {
             + "horsePower=" + horsePower
             + ", manufacturer='" + manufacturer + '\''
             + '}';
+    }
+
+    @Override
+    protected Engine clone() {
+        return new Engine(horsePower, manufacturer);
     }
 }
--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,9 +1,49 @@
 package core.basesyntax;
 
-public class Wheel {
+import java.util.Objects;
+
+public class Wheel implements Cloneable {
     private int radius;
 
-    //implement this class
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    protected Wheel clone() {
+        return new Wheel(radius);
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(radius);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+
+        if (obj == null) {
+            return false;
+        }
+
+        if (obj.getClass().equals(Wheel.class)) {
+            Wheel wheel = (Wheel) obj;
+            return wheel.getRadius() == this.getRadius()
+                    || obj.hashCode() == this.hashCode();
+        }
+        return false;
+    }
 
     @Override
     public String toString() {
