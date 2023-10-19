06ca6231c35a513be81abc7674eb7998fc3592be
Danil Ponomarenko <akame.dante13@gmail.com>
2023-10-19 16:48:07 +0300

    The first version of the program has been created.

--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,14 +1,57 @@
 package core.basesyntax;
 
-public class Wheel {
+import java.util.Objects;
+
+public class Wheel implements Cloneable{
     private int radius;
 
-    //implement this class
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public boolean equals(Object wheel) {
+        if (wheel == this) {
+            return true;
+        }
+        if (wheel == null) {
+            return false;
+        }
+        if (wheel.getClass().equals(Wheel.class)) {
+            Wheel current = (Wheel) wheel;
+            return Objects.equals(this.radius, current.radius);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result += 31 * result + radius;
+        return result;
+    }
+
+    @Override
+    protected Wheel clone() {
+        try {
+            return (Wheel) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't clone wheel properly. " + e);
+        }
+    }
 
     @Override
     public String toString() {
         return "Wheel{"
-            + "radius=" + radius
-            + '}';
+                + "radius=" + radius
+                + '}';
     }
 }
