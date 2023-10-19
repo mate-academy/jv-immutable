5d31f964a915d29e97e26c1eb4d45f39ac6b5d5d
Denys Okhrymovych <denys.ohrymovych.2015@gmail.com>
2023-10-19 20:26:41 +0300

    make Car class immutable, implement changeEngine(Engine engine), changeColor(String newColor), addWheel(Wheel newWheel)

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,10 +1,66 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
-    //implement this class
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + horsePower;
+        result = 31 * result + (manufacturer == null ? 0 : manufacturer.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object engine) {
+        if (engine == this) {
+            return true;
+        }
+        if (engine == null) {
+            return false;
+        }
+        if (engine.getClass().equals(Engine.class)) {
+            Engine current = (Engine) engine;
+            return this.horsePower == current.horsePower
+                    && (this.manufacturer == current.manufacturer
+                    || (this.manufacturer != null
+                    && this.manufacturer.equals(current.manufacturer)));
+        }
+        return false;
+    }
+
+    @Override
+    public Engine clone() {
+        try {
+            Engine clonedEngine = (Engine) super.clone();
+            clonedEngine.manufacturer = this.manufacturer;
+            return clonedEngine;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Clone of Engine object cannot be created", e);
+        }
+    }
 
     @Override
     public String toString() {
