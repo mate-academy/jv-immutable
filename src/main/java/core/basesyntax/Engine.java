c28bd59d8e19380331eb3699e3c3bdead21eea89
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2023-10-19 17:16:14 +0300

    implemented immutable Car class

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,10 +1,56 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
-    //implement this class
+    public Engine() {
+
+    }
+
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    public int getHorsePower() {
+        return this.horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public String getManufacturer() {
+        return this.manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int hashCode() {
+        int result = 17;
+        result = result * 31 + horsePower;
+        result = result * 31 + (manufacturer == null ? 0 : manufacturer.hashCode());
+        return result;
+    }
+
+    public boolean equals(Object engine) {
+        if (this == engine) {
+            return true;
+        }
+        if (engine == null) {
+            return false;
+        }
+
+        if (engine.getClass().equals(Engine.class)) {
+            Engine current = (Engine) engine;
+            return this.horsePower == current.horsePower
+                    && manufacturer != null && manufacturer.equals(current.manufacturer);
+        }
+        return false;
+    }
 
     @Override
     public String toString() {
@@ -12,5 +58,13 @@ public class Engine {
             + "horsePower=" + horsePower
             + ", manufacturer='" + manufacturer + '\''
             + '}';
+    }
+
+    public Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't clone engine ", e);
+        }
     }
 }
