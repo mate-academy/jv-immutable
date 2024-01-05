8c493ba8bd8e8ef8078aa9606c40dbabcaefff6d
Nazar Kolesnikov <nazarkolesnikov536@gmail.com>
2024-01-05 22:13:28 +0200

    Initial commit.

--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,14 +1,44 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class Wheel {
     private int radius;
 
-    //implement this class
+    /**
+     *  Implement this class.
+     */
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public Wheel clone() {
+        try {
+            return (Wheel) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(radius);
+    }
 
     @Override
     public String toString() {
         return "Wheel{"
-            + "radius=" + radius
+            + "radius="
+            + radius
             + '}';
     }
 }
