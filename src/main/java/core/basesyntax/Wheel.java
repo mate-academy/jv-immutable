541b6a33f374f32aa27c9f3446dedb8765eb5338
Bohdan <bohdan2704@gmail.com>
2024-01-23 19:02:41 +0200

    made all immutable funtionality

--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,7 +1,21 @@
 package core.basesyntax;
 
-public class Wheel {
+import java.util.Objects;
+
+public class Wheel implements Cloneable {
     private int radius;
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
 
     //implement this class
 
@@ -10,5 +24,31 @@ public class Wheel {
         return "Wheel{"
             + "radius=" + radius
             + '}';
+    }
+
+    @Override
+    public Wheel clone() {
+        // TODO: copy mutable state here, so the clone can't change the internals of the original
+        try {
+            return (Wheel) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Problems when cloning Wheel object", e);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Wheel wheel)) {
+            return false;
+        }
+        return getRadius() == wheel.getRadius();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getRadius());
     }
 }
