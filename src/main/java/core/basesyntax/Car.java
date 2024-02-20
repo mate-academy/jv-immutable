d3d8c901e9baf191bd165125233d13d0d7dd727e
Alex Naumenko <alex_naumenko_@ukr.net>
2024-02-20 13:17:33 +0200

    implemented immutable class Car

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,17 +1,96 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-/**
- * Make this class immutable. See requirements in task description.
- */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
 
-    //implement this class
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = new ArrayList<>();
+        for (Wheel wheel : wheels) {
+            this.wheels.add(new Wheel(wheel));
+        }
+        this.engine = engine != null
+                ? engine.clone()
+                : null;
+    }
+
+    public Car changeEngine(Engine engine) {
+        return new Car(year, color, wheels, engine.clone());
+    }
+
+    public Car changeColor(String newColor) {
+        return new Car(year, newColor, wheels, engine);
+    }
+
+    public Car addWheel(Wheel newWheel) {
+        List<Wheel> updatedWheels = new ArrayList<>(this.wheels);
+        updatedWheels.add(newWheel);
+        return new Car(this.year, this.color, updatedWheels, this.engine.clone());
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        List<Wheel> deepCopy = new ArrayList<>(wheels.size());
+        for (Wheel wheel : wheels) {
+            deepCopy.add(wheel.clone());
+        }
+
+        return deepCopy;
+    }
+
+    public Engine getEngine() {
+        if (engine == null) {
+            return null;
+        }
+
+        return engine.clone();
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + year;
+        result = 31 * result + (color == null ? 0 : color.hashCode());
+        result = 31 * result + (wheels == null ? 0 : wheels.hashCode());
+        result = 31 * result + (engine == null ? 0 : engine.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object car) {
+        if (car == this) {
+            return true;
+        }
+
+        if (car == null) {
+            return false;
+        }
+
+        if (car.getClass().equals(Car.class)) {
+            Car current = (Car) car;
+            return this.year == current.year
+                    && Objects.equals(this.color, current.color)
+                    && Objects.equals(this.wheels, current.wheels)
+                    && Objects.equals(this.engine, current.engine);
+        }
+
+        return false;
+    }
 
     @Override
     public String toString() {
