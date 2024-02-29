472c459b674d99eb899b41cb248b8306ded4f295
Maksym Shulypa <maximussamsung1@gmail.com>
2024-02-29 17:57:29 +0200

    done impl

--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,8 +1,43 @@
 package core.basesyntax;
 
-public class Wheel {
+import java.util.Objects;
+
+public final class Wheel implements Cloneable {
     private int radius;
 
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    protected Wheel clone() {
+        return new Wheel(radius);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Wheel wheel = (Wheel) obj;
+        return radius == ((Wheel) obj).radius;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(radius);
+    }
     //implement this class
 
     @Override
