daea1b1889ca7db85829e78d40f414a4e6dc776f
Hanna Ratushniak <ratushnyakhanna@gmail.com>
2024-03-05 00:23:45 +0200

    added nessesary methods

--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,9 +1,49 @@
 package core.basesyntax;
 
-public class Wheel {
+import java.util.Objects;
+
+public class Wheel implements Cloneable {
     private int radius;
 
-    //implement this class
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + radius;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object wheel) {
+        if (wheel == this) {
+            return true;
+        }
+        if (wheel == null || wheel.getClass() != Wheel.class) {
+            return false;
+        }
+        Wheel currentWheel = (Wheel)wheel;
+        return Objects.equals(this.radius,currentWheel.radius);
+    }
+
+    @Override
+    protected Wheel clone() {
+        try {
+            return (Wheel) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError("Clone not supported for engine");
+        }
+    }
 
     @Override
     public String toString() {
