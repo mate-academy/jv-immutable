99f93adc028169a3838338b32b1ea274bf62d2a8
Denys <Eidenn005@gmail.com>
2024-03-06 18:10:19 +0200

    java-immutable solution Implements Car class with all needed fields Implements Engine and Wheel classes

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,15 +1,24 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Make this class immutable. See requirements in task description.
  */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
+
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = getCopy(wheels);
+        this.engine = (engine == null) ? null : engine.clone();
+    }
 
     //implement this class
 
@@ -21,5 +30,61 @@ public class Car {
             + ", wheels=" + wheels
             + ", engine=" + engine
             + '}';
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        return getCopy(wheels);
+    }
+
+    public Engine getEngine() {
+        if (engine == null) {
+            return null;
+        }
+        return engine.clone();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return year == car.year && Objects.equals(color, car.color)
+                && Objects.equals(wheels, car.wheels)
+                && Objects.equals(engine, car.engine);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(year, color, wheels, engine);
+    }
+
+    private static List<Wheel> getCopy(List<Wheel> wheels) {
+        List<Wheel> wheelsCopy = new ArrayList<>(wheels.size());
+        for (Wheel wheel : wheels) {
+            wheelsCopy.add(wheel.clone());
+        }
+        return wheelsCopy;
+    }
+
+    public Car changeEngine(Engine newEngine) {
+        return new Car(getYear(), getColor(), getCopy(wheels), newEngine.clone());
+    }
+
+    public Car changeColor(String newColor) {
+        return new Car(getYear(), newColor, getCopy(wheels), engine);
+    }
+
+    public Car addWheel(Wheel newWheel) {
+        List<Wheel> newWheels = new ArrayList<>(wheels);
+        newWheels.add(newWheel);
+        return new Car(year, color, newWheels, engine);
     }
 }
