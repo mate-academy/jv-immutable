47cdeaa2c1a6b1d4c6506c9d31e6d1f8eb38c4d5
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-03-07 08:41:16 +0200

    Engine.java

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,16 +1,23 @@
 package core.basesyntax;
 
-public class Engine implements Cloneable {
+public class Engine {
     private int horsePower;
     private String manufacturer;
 
-    @Override
-    public Engine clone() {
-        try {
-            return (Engine) super.clone();
-        } catch (CloneNotSupportedException e) {
-            throw new AssertionError();
-        }
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
     }
 
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public Engine clone() {
+        return new Engine(this.horsePower, this.manufacturer);
+    }
 }
