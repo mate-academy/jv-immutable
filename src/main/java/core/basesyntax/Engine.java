1d62969129975c6f95178a64069fb7f21a7955d5
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-03-07 09:15:28 +0200

    Engine.java

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,8 +1,8 @@
 package core.basesyntax;
 
 public class Engine implements Cloneable {
-    private final int horsePower;
-    private final String manufacturer;
+    private int horsePower;
+    private String manufacturer;
 
     public Engine(int horsePower, String manufacturer) {
         this.horsePower = horsePower;
@@ -18,11 +18,33 @@ public class Engine implements Cloneable {
     }
 
     @Override
-    public Engine clone() {
+    protected Engine clone() {
         try {
             return (Engine) super.clone();
         } catch (CloneNotSupportedException e) {
             throw new AssertionError();
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Engine engine = (Engine) o;
+        return horsePower == engine.horsePower &&
+                Objects.equals(manufacturer, engine.manufacturer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(horsePower, manufacturer);
+    }
+
+    @Override
+    public String toString() {
+        return "Engine{" +
+                "horsePower=" + horsePower +
+                ", manufacturer='" + manufacturer + '\'' +
+                '}';
     }
 }
