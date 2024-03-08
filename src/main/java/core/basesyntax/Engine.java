f308bcf27fab9f576ea5e6237a3d4532c327a7fe
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-03-08 08:29:10 +0200

    Engine.java

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,26 +1,11 @@
 package core.basesyntax;
 
-import java.util.Objects;
-
 public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
-    public Engine(int horsePower, String manufacturer) {
-        this.horsePower = horsePower;
-        this.manufacturer = manufacturer;
-    }
-
-    public int getHorsePower() {
-        return horsePower;
-    }
-
-    public String getManufacturer() {
-        return manufacturer;
-    }
-
     @Override
-    protected Engine clone() {
+    public Engine clone() {
         try {
             return (Engine) super.clone();
         } catch (CloneNotSupportedException e) {
@@ -29,28 +14,10 @@ public class Engine implements Cloneable {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Engine engine = (Engine) o;
-        return horsePower == engine.horsePower
-                && Objects.equals(manufacturer, engine.manufacturer);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(horsePower, manufacturer);
-    }
-
-    @Override
     public String toString() {
-        return "Engine{"
-                + "horsePower=" + horsePower
-                + ", manufacturer='" + manufacturer + '\''
-                + '}';
+        return "Engine{" +
+            "horsePower=" + horsePower +
+            ", manufacturer='" + manufacturer + '\'' +
+            '}';
     }
 }
