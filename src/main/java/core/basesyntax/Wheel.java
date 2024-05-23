d6dc2dbc7450156d8139eddce45c401f561257d8
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-05-23 22:14:28 +0300

    Added solution of immutable Car

--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,14 +1,49 @@
 package core.basesyntax;
 
-public class Wheel {
+public final class Wheel implements Cloneable {
+    private final String CLONE_ERROR = "Failed to clone wheel";
     private int radius;
 
-    //implement this class
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    protected Wheel clone() {
+        try {
+            return (Wheel) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(CLONE_ERROR, e);
+        }
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
 
     @Override
     public String toString() {
         return "Wheel{"
             + "radius=" + radius
             + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return radius * CLONE_ERROR.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        return obj instanceof Wheel castedObject &&
+                (this == castedObject
+                        || this.radius == castedObject.radius);
     }
 }
