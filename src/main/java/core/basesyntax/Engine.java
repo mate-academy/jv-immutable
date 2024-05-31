01d92cd1f2c9543c8b0e2a05f35ec0a0ec1773c6
Dmytro Lapikov <it.lapikov@gmail.com>
2024-05-31 17:07:54 +0300

    task done, test passed

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,10 +1,49 @@
 package core.basesyntax;
 
-public class Engine {
+import java.util.Objects;
+
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
-    //implement this class
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(horsePower, manufacturer);
+    }
+
+    @Override
+    public boolean equals(Object engine) {
+        if (this == engine) {
+            return true;
+        }
+        if (engine == null || getClass() != engine.getClass()) {
+            return false;
+        }
+        Engine newEngine = (Engine) engine;
+        return horsePower == newEngine.horsePower
+                && Objects.equals(manufacturer, newEngine.manufacturer);
+    }
 
     @Override
     public String toString() {
@@ -12,5 +51,14 @@ public class Engine {
             + "horsePower=" + horsePower
             + ", manufacturer='" + manufacturer + '\''
             + '}';
+    }
+
+    @Override
+    protected Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can`t clone object, because its not cloneable. ", e);
+        }
     }
 }
