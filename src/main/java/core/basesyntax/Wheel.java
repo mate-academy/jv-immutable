e125f99b175e0945b8a623602e59db0710815f74
unknown <kykyangelina182@gmail.com>
2024-06-11 15:14:34 +0300

    Solution

--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,14 +1,50 @@
 package core.basesyntax;
 
-public class Wheel {
+public class Wheel implements Cloneable {
     private int radius;
 
-    //implement this class
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
 
     @Override
     public String toString() {
         return "Wheel{"
-            + "radius=" + radius
-            + '}';
+                + "radius=" + radius
+                + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(radius);
+    }
+
+    @Override
+    public boolean equals(Object wheel) {
+        if (this == wheel) {
+            return true;
+        }
+        if (wheel == null || getClass() != wheel.getClass()) {
+            return false;
+        }
+        Wheel newWheel = (Wheel) wheel;
+        return radius == newWheel.radius;
+    }
+
+    @Override
+    protected Wheel clone() {
+        try {
+            return (Wheel) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can`t clone object, because its not cloneable. ", e);
+        }
     }
 }
