d4c70e9d27475cdee33420c3eff18438b9195241
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-06-28 07:51:24 +0300

    Wheel.java

--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,14 +1,44 @@
 package core.basesyntax;
 
-public class Wheel {
-    private int radius;
+import java.util.Objects;
 
-    //implement this class
+public class Wheel implements Cloneable {
+    private final int radius;
+
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    @Override
+    public Wheel clone() {
+        try {
+            return (Wheel) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError("Cloning not supported", e);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Wheel wheel = (Wheel) o;
+        return radius == wheel.radius;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(radius);
+    }
 
     @Override
     public String toString() {
-        return "Wheel{"
-            + "radius=" + radius
-            + '}';
+        return "Wheel{" +
+                "radius=" + radius +
+                '}';
     }
 }
