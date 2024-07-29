4f8b72e98639725c7b9c2f8953da624f8b9579ff
KLICKICE <ledmaskiight@gmail.com>
2024-07-29 18:37:51 +0300

    Check pls, what shoud i do?

--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,14 +1,52 @@
 package core.basesyntax;
 
-public class Wheel {
-    private int radius;
+import java.util.Objects;
 
-    //implement this class
+public class Wheel implements Cloneable {
+    private final int radius;
+
+    public Wheel(Wheel wheel) {
+        this(wheel.radius);
+    }
+
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    @Override
+    protected Wheel clone() {
+        try {
+            return (Wheel) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't clone Wheel", e);
+        }
+    }
 
     @Override
     public String toString() {
         return "Wheel{"
-            + "radius=" + radius
-            + '}';
+                + "radius=" + radius
+                + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(radius);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Wheel current = (Wheel) o;
+        return radius == current.radius;
     }
 }
