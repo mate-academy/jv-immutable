cf8ed4bec689043f535ff401732dac61700af006
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-08-09 21:25:04 +0300

    Created: -constructor, -getters and setters, Override methods: -equals and hashcode, -clone, -toString;

--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,14 +1,53 @@
 package core.basesyntax;
 
-public class Wheel {
+import java.util.Objects;
+
+public final class Wheel implements Cloneable {
     private int radius;
 
-    //implement this class
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Wheel wheel = (Wheel) o;
+        return getRadius() == wheel.getRadius();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getRadius());
+    }
+
+    @Override
+    protected Wheel clone() {
+        try {
+            return (Wheel) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Override
     public String toString() {
         return "Wheel{"
-            + "radius=" + radius
-            + '}';
+                + "radius=" + radius
+                + '}';
     }
+
 }
