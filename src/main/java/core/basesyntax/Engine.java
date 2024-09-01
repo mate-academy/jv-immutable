cd4aab889f88b632119e4a60196037fa7404c0f4
skviropitek <skvyra.anton@gmail.com>
2024-09-01 18:59:36 +0200

    created solution

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,16 +1,52 @@
 package core.basesyntax;
 
-public class Engine {
-    private int horsePower;
-    private String manufacturer;
+import java.util.Objects;
 
-    //implement this class
+public final class Engine implements Cloneable {
+    private final int horsePower;
+    private final String manufacturer;
+
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
 
     @Override
-    public String toString() {
-        return "Engine{"
-            + "horsePower=" + horsePower
-            + ", manufacturer='" + manufacturer + '\''
-            + '}';
+    protected Engine clone() {
+        return new Engine(horsePower, manufacturer);
+    }
+
+    public Engine setHorsePower(int newHorsePower) {
+        return new Engine(newHorsePower, this.manufacturer);
+    }
+
+    public Engine setManufacturer(String newManufacturer) {
+        return new Engine(this.horsePower, newManufacturer);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Engine engine = (Engine) o;
+        return horsePower == engine.horsePower
+                && Objects.equals(manufacturer, engine.manufacturer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(horsePower, manufacturer);
     }
 }
--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,14 +1,48 @@
 package core.basesyntax;
 
-public class Wheel {
-    private int radius;
+import java.util.Objects;
 
-    //implement this class
+public class Wheel implements Cloneable {
+    private final int radius;
+
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    @Override
+    protected Wheel clone() {
+        return new Wheel(radius);
+    }
+
+    public Wheel setRadius(int newRadius) {
+        return new Wheel(newRadius);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Wheel wheel = (Wheel) o;
+        return radius == wheel.radius;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(radius);
+    }
 
     @Override
     public String toString() {
         return "Wheel{"
-            + "radius=" + radius
-            + '}';
+                + "radius="
+                + radius + '}';
     }
 }
