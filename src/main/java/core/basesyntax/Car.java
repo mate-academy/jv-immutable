b90e2c84896c5d0b6815b5d7855ecc5536f205d7
Alisa Hoys <hoys@protonmail.com>
2024-10-03 16:18:56 +0300

    Implemented an immutable Car class, Engine and Wheel classes.

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,17 +1,88 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-/**
- * Make this class immutable. See requirements in task description.
- */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car implements Cloneable {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
 
-    //implement this class
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = (color != null) ? color : "";
+        this.engine = (engine != null) ? engine.clone() : null;
+        this.wheels = new ArrayList<>();
+        if (wheels == null) {
+            throw new NullPointerException("Wheels list cannot be null.");
+        }
+        for (Wheel wheel : wheels) {
+            if (wheel != null) {
+                this.wheels.add(wheel.clone());
+            }
+        }
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Wheel> getWheels() {
+        List<Wheel> newWheels = new ArrayList<>();
+        for (Wheel wheel : this.wheels) {
+            if (wheel != null) {
+                newWheels.add(wheel.clone());
+            }
+        }
+        return newWheels;
+    }
+
+    public Engine getEngine() {
+        return (engine != null) ? engine.clone() : null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return year == car.year
+                && Objects.equals(color, car.color)
+                && Objects.equals(wheels, car.wheels)
+                && Objects.equals(engine, car.engine);
+    }
+
+    public Car changeEngine(Engine newEngine) {
+        return new Car(this.year, this.color, this.wheels, newEngine);
+    }
+
+    public Car changeColor(String newColor) {
+        if (newColor == null) {
+            throw new IllegalArgumentException("Color cannot be null");
+        }
+        return new Car(this.year, newColor, this.wheels, this.engine);
+    }
+
+    public Car addWheel(Wheel newWheel) {
+        List<Wheel> newWheels = new ArrayList<>(this.wheels);
+        newWheels.add(newWheel);
+        return new Car(this.year, this.color, newWheels, this.engine);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(year, color, wheels, engine);
+    }
 
     @Override
     public String toString() {
