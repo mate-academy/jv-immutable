2df8f09668a1f3be11f24d134998afae0fffc3c1
iurii <kenu21@gmail.com>
2024-10-10 19:43:03 +0200

    CREATE-IMMUTABLE: implemented immutable class Car

--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,14 +1,48 @@
 package core.basesyntax;
 
-public class Wheel {
+public class Wheel implements Cloneable {
+
     private int radius;
 
-    //implement this class
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
 
     @Override
     public String toString() {
         return "Wheel{"
             + "radius=" + radius
             + '}';
+    }
+
+    @Override
+    protected Wheel clone() {
+        return new Wheel(radius);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+
+        Wheel wheel = (Wheel) object;
+        return getRadius() == wheel.getRadius();
+    }
+
+    @Override
+    public int hashCode() {
+        return getRadius();
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
     }
 }
