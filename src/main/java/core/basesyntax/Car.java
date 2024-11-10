86bb41ae1d866c6e006152c4945bfd2c82db9762
Vasyl <vasiliy.azarov.vl1@gmail.com>
2024-11-10 21:33:14 +0100

    fix bugs 2

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -71,7 +71,8 @@ public final class Car {
             return false;
         }
         Car car = (Car) o;
-        return year == car.year && Objects.equals(color, car.color) && Objects.equals(wheels, car.wheels) && Objects.equals(engine, car.engine);
+        return year == car.year && Objects.equals(color, car.color)
+                && Objects.equals(wheels, car.wheels) && Objects.equals(engine, car.engine);
     }
 
     @Override
