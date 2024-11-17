32779de571a8a9e93fc2c9f55b1b14f3f17efeea
Pavlo Sukhanko <swtlouisiana@gmail.com>
2024-11-17 22:47:42 +0000

    implementation all methods and classes

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -4,10 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-/**
- * Make this class immutable. See requirements in task description.
- */
-public class Car {
+public final class Car implements Cloneable {
     private final int year;
     private final String color;
     private final List<Wheel> wheels;
