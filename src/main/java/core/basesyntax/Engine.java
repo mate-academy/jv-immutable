dd579db6879a0cf264c4ad3bedd0e53ca7bcc83e
DariaBakal <bakal.dasha01@gmail.com>
2024-11-26 23:48:52 +0200

    implemented immutable class Car

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,16 +1,66 @@
 package core.basesyntax;
 
-public class Engine {
+import java.util.Objects;
+
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
-    //implement this class
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + horsePower;
+        result = 31 * result + (manufacturer == null ? 0 : manufacturer.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof Engine current)) {
+            return false;
+        }
+        return Objects.equals(horsePower, current.horsePower)
+                && Objects.equals(manufacturer, current.manufacturer);
+    }
+
+    @Override
+    protected Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Cant create clone of Engine object", e);
+        }
+    }
 
     @Override
     public String toString() {
         return "Engine{"
-            + "horsePower=" + horsePower
-            + ", manufacturer='" + manufacturer + '\''
-            + '}';
+                + "horsePower=" + horsePower
+                + ", manufacturer='" + manufacturer + '\''
+                + '}';
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
     }
 }
