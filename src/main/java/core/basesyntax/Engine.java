87ac1826006c16a53acb4dd963dd9c6766830ad6
Dmytro Borysenko <borisand020774@gmail.com>
2024-12-06 17:45:31 +0200

    https://github.com/DmytroBorysenko2004/jv-immutable

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,19 +1,42 @@
 package core.basesyntax;
 
-public class Engine {
+import java.util.Objects;
+
+public final class Engine {
     private final int horsePower;
     private final String manufacturer;
 
-    public Engine(int horsePower , String manufacturer) {
+    public Engine(int horsePower, String manufacturer) {
         this.horsePower = horsePower;
         this.manufacturer = manufacturer;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Engine engine = (Engine) o;
+        return horsePower == engine.horsePower && manufacturer.equals(engine.manufacturer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(horsePower, manufacturer);
     }
 
     @Override
     public String toString() {
         return "Engine{"
-            + "horsePower=" + horsePower
-            + ", manufacturer='" + manufacturer + '\''
-            + '}';
+                + "horsePower=" + horsePower
+                + ", manufacturer='" + manufacturer + '\''
+                + '}';
     }
 }
