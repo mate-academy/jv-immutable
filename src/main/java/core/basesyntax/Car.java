6aadc9d7167aeb18d6559bfa9662170b57a52d3c
Dmytro Borysenko <borisand020774@gmail.com>
2024-12-06 17:42:43 +0200

    https://github.com/DmytroBorysenko2004/jv-immutable

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-/**
- * Make this class immutable. See requirements in task description.
- */
 public class Car {
     private final Integer year;
     private final String color;
