d90b08e520317ffc023c55c602af407774fb05e5
Yaroslav Narozhnyi <gnredit9@gmail.com>
2024-12-14 20:01:49 +0200

    574898

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,10 +1,15 @@
 package core.basesyntax;
 
-public class Engine {
+import java.util.Objects;
+
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
-    //implement this class
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
 
     @Override
     public String toString() {
@@ -12,5 +17,53 @@ public class Engine {
             + "horsePower=" + horsePower
             + ", manufacturer='" + manufacturer + '\''
             + '}';
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    @Override
+    public Engine clone() {
+        try {
+            Engine engineNew = (Engine) super.clone();
+            return engineNew;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can not clone", e);
+        }
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + horsePower;
+        result = 31 * result + ((manufacturer == null) ? 0
+                : manufacturer.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj != null && getClass() == obj.getClass()) {
+            Engine other = (Engine) obj;
+            return horsePower == other.horsePower && Objects.equals(manufacturer, other.manufacturer);
+        }
+        return false;
+    }
+
+    public void setHorsePower(int i) {
+        this.horsePower = i;
+    }
+
+    public void setManufacturer(String newMaker) {
+        this.manufacturer = newMaker;
     }
 }
--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,14 +1,52 @@
 package core.basesyntax;
 
-public class Wheel {
+public class Wheel implements Cloneable {
     private int radius;
 
-    //implement this class
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
 
     @Override
     public String toString() {
         return "Wheel{"
             + "radius=" + radius
             + '}';
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    @Override
+    public Wheel clone() {
+        try {
+            return (Wheel) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can not clone object",e);
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + (radius);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Wheel current = (Wheel) obj;
+        return radius == current.radius;
+    }
+
+    public void setRadius(int i) {
+        this.radius = i;
     }
 }
