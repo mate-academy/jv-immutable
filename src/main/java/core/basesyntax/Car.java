b9f594c6265c65a433b4a5ffb5b1b71a8f1084b3
Teslenko Daniil <burgalhashem@gmail.com>
2024-12-17 20:48:50 +0200

    implement immutable

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-
 public final class Car {
     private final int year;
     private final String color;
