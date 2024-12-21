268ecbbc8d27ddedb5a30c17db3078d71221cfe1
Sasha Koval <kovalsana40@gmail.com>
2024-12-21 09:52:04 +0200

    try number one

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,17 +1,42 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Make this class immutable. See requirements in task description.
  */
-public class Car {
+public final class Car implements Cloneable {
     private int year;
     private String color;
     private List<Wheel> wheels;
     private Engine engine;
 
     //implement this class
+
+
+
+
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = new ArrayList<>(getWheels());
+        this.engine = engine.clone();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return year == car.year && Objects.equals(color, car.color) && Objects.equals(wheels, car.wheels) && Objects.equals(engine, car.engine);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(year, color, wheels, engine);
+    }
 
     @Override
     public String toString() {
@@ -21,5 +46,58 @@ public class Car {
             + ", wheels=" + wheels
             + ", engine=" + engine
             + '}';
+    }
+
+    public Engine getEngine() {
+        return engine.clone();
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+
+    public Car changeColor(String blue) {
+        try {
+            Car updatedCar = (Car) super.clone();
+            updatedCar.color = blue;
+            return updatedCar;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public List<Wheel> getWheels() {
+        List<Wheel> copyWheels = new ArrayList<>(wheels.size());
+        for (Wheel wheel : wheels) {
+            copyWheels.add(wheel.clone());
+        }
+        return copyWheels;
+    }
+
+    public Car addWheel(Wheel wheel) {
+        try {
+            Car updatedCar = (Car) super.clone();
+            updatedCar.wheels.add(wheel);
+            return updatedCar;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public Car changeEngine(Engine otherMaker) {
+        try {
+            Car updatedCar = (Car) super.clone();
+            updatedCar.engine = otherMaker;
+            return updatedCar;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,10 +1,39 @@
 package core.basesyntax;
 
-public class Engine {
+import java.util.Objects;
+
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
     //implement this class
+
+
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Engine engine = (Engine) o;
+        return horsePower == engine.horsePower && Objects.equals(manufacturer, engine.manufacturer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(horsePower, manufacturer);
+    }
 
     @Override
     public String toString() {
@@ -12,5 +41,22 @@ public class Engine {
             + "horsePower=" + horsePower
             + ", manufacturer='" + manufacturer + '\''
             + '}';
+    }
+
+    public void setHorsePower(int i) {
+        this.horsePower = i;
+    }
+
+    public void setManufacturer(String newMaker) {
+        this.manufacturer = newMaker;
+    }
+
+    @Override
+    protected Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
