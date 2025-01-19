10605ac76c0ff251b9abfbe6d276e75e2a5ed44d
Mykola <mykola.turak.ki.2023@lpnu.ua>
2025-01-19 11:36:16 +0200

    make class Car immutable

--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,10 +1,46 @@
 package core.basesyntax;
 
-public class Wheel {
+import java.util.Objects;
+
+public class Wheel implements Cloneable {
     private int radius;
 
-    //implement this class
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
 
+    public Wheel(Wheel wheel) {
+        this.radius = wheel.getRadius();
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Wheel wheel)) {
+            return false;
+        }
+
+        return radius == wheel.radius;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(radius);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    //implement this class
     @Override
     public String toString() {
         return "Wheel{"
