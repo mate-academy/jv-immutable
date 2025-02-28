d484137d9b140266ddb5c75db56d95cc9ff2fcf2
anton <anton.surzhok@gmail.com>
2025-02-28 10:21:20 +0200

    Test

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 import java.util.Objects;
 
-public final class Engine {
+public final class Engine implements Cloneable {
     private final int horsePower;
     private final String manufacturer;
 
@@ -22,6 +22,15 @@ public final class Engine {
 
     public String getManufacturer() {
         return manufacturer;
+    }
+
+    @Override
+    public Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError(); // Не повинно траплятися, оскільки ми реалізуємо Cloneable
+        }
     }
 
     @Override
